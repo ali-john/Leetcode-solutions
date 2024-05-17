438696015d2b0b19bb24b1f8a87b46b695771b5f
Ali John <63426759+ali-john@users.noreply.github.com>
2024-05-17 20:04:23 +0000

    [LeetCode Sync] Runtime - 118 ms (81.08%), Memory - 21.4 MB (7.34%)

---
+++ my-folder/0056-merge-intervals/solution.py
@@ -0,0 +1,17 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        sorted_list = sorted(intervals,key= lambda x:(x[0]))
+        output = []
+        i = 0
+        while(i<len(sorted_list)):
+            start = i
+            max_end = sorted_list[i][1]
+            while i+1<len(sorted_list) and sorted_list[i+1][0]<=max_end:
+                max_end = max(max_end,sorted_list[i+1][1])
+                i+=1
+            l = []
+            l.append(sorted_list[start][0])
+            l.append(max_end)
+            output.append(l)
+            i+=1
+        return output
