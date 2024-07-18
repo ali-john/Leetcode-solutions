75dcd3a9f09aa5c1c84dedc194a0238395cd9a39
Ali John <63426759+ali-john@users.noreply.github.com>
2024-07-18 17:55:51 +0000

    [LeetCode Sync] Runtime - 33 ms (73.95%), Memory - 16.5 MB (75.83%)

--- my-folder/0092-reverse-linked-list-ii/solution.py
+++ my-folder/0092-reverse-linked-list-ii/solution.py
@@ -1,31 +1,26 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution:
     def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
-        # More efficient way would be using two pointers approach
-        values = []
-        temp = head
-        i = 1
-        while temp is not None:
-            if i>=left and i<=right:
-                values.append(temp.val)
-            i+=1
-            temp = temp.next if temp is not None else None
-
-        i = 1
-        temp = head
-        j = len(values)-1
-        while temp is not None:
-            if i>=left and i<=right:
-                temp.val = values[j]
-                j-=1
-            i+=1
-            temp = temp.next if temp is not None else None
-        return head
-
-            
-
+        if not head or left == right:
+            return head
         
+        dummy = ListNode(0)
+        dummy.next = head
+        prev = dummy
+        
+        # Step 1: Reach the node just before the "left" position
+        for _ in range(left - 1):
+            prev = prev.next
+        
+        # `start` will eventually be the tail of the reversed sublist
+        start = prev.next
+        then = start.next
+        
+        # Step 2: Reverse the sublist from "left" to "right"
+        for _ in range(right - left):
+            start.next = then.next
+            then.next = prev.next
+            prev.next = then
+            then = start.next
+        
+        return dummy.next
+
