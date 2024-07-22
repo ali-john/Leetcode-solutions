b705abaae5c057a999d152b73cbd179f957afa77
Ali John <63426759+ali-john@users.noreply.github.com>
2024-07-22 23:48:24 +0000

    [LeetCode Sync] Runtime - 33 ms (78.02%), Memory - 16.8 MB (36.54%)

--- my-folder/0103-binary-tree-zigzag-level-order-traversal/solution.py
+++ my-folder/0103-binary-tree-zigzag-level-order-traversal/solution.py
@@ -11,47 +11,25 @@ class Solution:
         
         q = []
         q.append(root)
-        left = []
-        right = []
         output = []
-        j=0
-        while(len(q)>0):
+        leftToRight = True
+        while len(q)>0:
             size = len(q)
-            sub_list = []
-            for i in range(size):
+            level = []
+            for _ in range(size):
                 root = q.pop(0)
-                sub_list.append(root.val)
+                if leftToRight:
+                    level.append(root.val)
+                else:
+                    level.insert(0,root.val)
                 if root.left is not None:
                     q.append(root.left)
                 if root.right is not None:
                     q.append(root.right)
-            if j%2 ==0: #0,2,4,6 ...
-                right.append(sub_list)
-            else:
-                sub_list = sub_list[::-1]
-                left.append(sub_list)
-            j+=1
-        if len(right) == len(left):
-            for i in range(len(right)):
-                output.append(right[i])
-                output.append(left[i])
-        elif len(right)>len(left):
-            start = len(left)
-            for i in range(len(left)):
-                output.append(right[i])
-                output.append(left[i])
-            for i in range(start,len(right),1):
-                output.append(right[i])
-        
-        elif len(left)>len(right):
-            start = len(right)
-            for i in range(len(right)):
-                output.append(right[i])
-                output.append(left[i])
-            for i in range(start,len(left),1):
-                output.append(left[i])
-
-
-
+            output.append(level)
+            leftToRight = not leftToRight
         return output
-        
+                    
+                    
+                
+
