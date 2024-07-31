cc0deac671efb5cefa3a2766f421b38da7f9eaec
Ali John <63426759+ali-john@users.noreply.github.com>
2024-07-31 20:05:42 +0000

    [LeetCode Sync] Runtime - 79 ms (34.68%), Memory - 17.9 MB (30.74%)

--- my-folder/0034-find-first-and-last-position-of-element-in-sorted-array/solution.py
+++ my-folder/0034-find-first-and-last-position-of-element-in-sorted-array/solution.py
@@ -1,33 +1,39 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        def find_first():
-            left, right = 0, len(nums) - 1
-            while left <= right:
-                mid = (left + right) // 2
-                if nums[mid] < target:
-                    left = mid + 1
+        def search(find_max_index):
+            index = -1
+            start = 0
+            end = len(nums)-1
+
+            while start<=end:
+                middle = (start+end)//2
+                if nums[middle]<target:
+                    start = middle+1
+                elif nums[middle]>target:
+                    end = middle -1
+                
                 else:
-                    right = mid - 1
-            return left
+                    index = middle
+                    if find_max_index:
+                        start = middle+1
+                    else:
+                        end = middle -1
+            return index
 
-        def find_last():
-            left, right = 0, len(nums) - 1
-            while left <= right:
-                mid = (left + right) // 2
-                if nums[mid] <= target:
-                    left = mid + 1
-                else:
-                    right = mid - 1
-            return right
 
-        start = find_first()
-        end = find_last()
+        result = [-1,-1]
+        result[0] = search(False)
+        if result[0]!=-1:
+            result[1] = search(True)
+        
+        return result
+        
+        
+            
 
-        # Check if the target is not present in the array
-        if start <= end and start < len(nums) and nums[start] == target:
-            return [start, end]
-        else:
-            return [-1, -1]
+
+        
+            
 
                 
 
