29e93d21d74dfe519537ed01487601d06ed3d0ac
Ali John <63426759+ali-john@users.noreply.github.com>
2024-07-31 23:25:29 +0000

    [LeetCode Sync] Runtime - 48 ms (23.93%), Memory - 16.8 MB (95.16%)

--- my-folder/0033-search-in-rotated-sorted-array/solution.py
+++ my-folder/0033-search-in-rotated-sorted-array/solution.py
@@ -1,42 +1,29 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        if len(nums)==1:
-            return 0 if target==nums[0] else -1
-        pivot_point = 0
+        left = 0
+        right = len(nums)-1
 
-
-        def find_pivot():
-            left = 0
-            right = len(nums)-1
-
-            while left<right:
-                mid = (left+right)//2
-
-                if nums[mid]>nums[right]:
-                    left = mid+1
-                else:
-                    right = mid
-            return left
-                
-        def search_now(left,right):
-
-            while left<=right:
-                mid = (left+right)//2
-                if nums[mid] == target:
-                    return mid
-                
-                elif nums[mid]<target:
-                    left = mid+1
-                else:
-                    right = mid-1
+        while left<=right:
+            middle = (left+right)//2
+            if nums[middle]==target:
+                return middle
             
-            return -1
+            if nums[left]<=nums[middle]:
+                if target>nums[middle] or target<nums[left]:
+                    left = middle+1
+                else:
+                    right = middle-1
+            else:
+                if target<nums[middle] or target>nums[right]:
+                    right = middle -1
+                else:
+                    left = middle+1
 
-        pivot = find_pivot()
-        if nums[pivot]<=target<=nums[-1]:
-            return search_now(pivot,len(nums)-1)
-        else:
-            return search_now(0,pivot-1)
+        return -1
+
+
+
+        
     
         
                 
