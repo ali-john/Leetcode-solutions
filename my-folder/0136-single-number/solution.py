0fca77f10651d22ab78f0c8020f91a0aba51ea67
Ali John <63426759+ali-john@users.noreply.github.com>
2024-08-06 20:40:54 +0000

    [LeetCode Sync] Runtime - 113 ms (43.87%), Memory - 19 MB (51.19%)

--- my-folder/0136-single-number/solution.py
+++ my-folder/0136-single-number/solution.py
@@ -1,11 +1,13 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        if len(nums)==1:
-            return nums[0]
-        output = nums[0]
-        for i in range(1,len(nums)):
-            output^=nums[i]
+        output = 0
+        for i in range(len(nums)):
+            output ^= nums[i]
         return output
+
+
+
+        
 
 
         
