7cbe5827916dc614ea1de4686578047bab83358a
Ali John <63426759+ali-john@users.noreply.github.com>
2024-08-22 21:14:36 +0000

    [LeetCode Sync] Runtime - 360 ms (46.10%), Memory - 33.3 MB (70.49%)

---
+++ my-folder/1604-least-number-of-unique-integers-after-k-removals/solution.py
@@ -0,0 +1,25 @@
+import heapq
+class Solution:
+    def findLeastNumOfUniqueInts(self, arr: List[int], k: int) -> int:
+        table = defaultdict(int)
+        for num in arr:
+            table[num] += 1
+        
+        heap = []
+        for val in table.values():
+            heapq.heappush(heap, val)
+        
+        while k > 0:
+            min_freq = heapq.heappop(heap)
+            if k >= min_freq:
+                k -= min_freq
+            else:
+                heapq.heappush(heap, min_freq - k)
+                break
+        
+        return len(heap)
+
+        
+
+
+
