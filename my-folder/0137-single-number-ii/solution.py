f7b1e9d8d69a542047c9b45a386a4d7870354622
Ali John <63426759+ali-john@users.noreply.github.com>
2024-08-26 19:23:58 +0000

    [LeetCode Sync] Runtime - 53 ms (79.79%), Memory - 18.6 MB (75.34%)

--- my-folder/0137-single-number-ii/solution.py
+++ my-folder/0137-single-number-ii/solution.py
@@ -3,12 +3,9 @@ class Solution:
         ones = 0
         twos = 0
 
-        for i in range(len(nums)):
-            ones = ones^nums[i]
-            ones = ~(twos)&ones
-
-            twos = twos^nums[i]
-            twos = ~(ones)&twos
+        for num in nums:
+            ones = (ones^num)&~(twos)
+            twos = (twos^num)&~(ones)
         return ones
         
         
