1caa14fa439323f306d4b7a0d4abc65171d1283b
Ali John <63426759+ali-john@users.noreply.github.com>
2024-08-26 19:21:06 +0000

    [LeetCode Sync] Runtime - 108 ms (46.47%), Memory - 17.1 MB (99.91%)

--- my-folder/3493-maximum-number-of-operations-to-move-ones-to-the-end/solution.py
+++ my-folder/3493-maximum-number-of-operations-to-move-ones-to-the-end/solution.py
@@ -1,18 +1,14 @@
 import copy
 class Solution:
     def maxOperations(self, s: str) -> int:
-        string = [int(''.join(char)) for char in s]
-        n = len(string)
         oc = 0
-        zc = 0
-        output =0
-        for i in range(len(string)-1):
-            if string[i]==1:
+        operations = 0
+        for i in range(len(s)-1):
+            if s[i] == '1':
                 oc+=1
-            if string[i]==1 and string[i+1]==0:
-                output+=oc
-        return output
-
+            if s[i]=='1' and s[i+1]=='0':
+                operations+=oc
+        return operations    
 
 
 
