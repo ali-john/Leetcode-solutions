5c96e23d583517ce09e47953c0d9293d695c3eff
Ali John <63426759+ali-john@users.noreply.github.com>
2024-08-28 17:11:11 +0000

    [LeetCode Sync] Runtime - 463 ms (16.18%), Memory - 27.3 MB (55.72%)

---
+++ my-folder/0310-minimum-height-trees/solution.py
@@ -0,0 +1,32 @@
+class Solution:
+    def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
+        if n==1:
+            return [0]
+        graph = defaultdict(list)
+        degrees = [0]*n
+        # fill graph
+        for node1, node2 in edges:
+            graph[node1].append(node2)
+            graph[node2].append(node1)
+            degrees[node1]+=1
+            degrees[node2]+=1
+        print(degrees)
+        # store all leaf nodes
+        sources = []
+        for i in range(len(degrees)):
+            if degrees[i]==1: 
+                sources.append(i)
+        total_nodes = n
+        while total_nodes>2:
+            leaves = len(sources)
+            total_nodes-=leaves
+            for i in range(leaves):
+                vertex = sources.pop(0)
+                for neighbour in graph[vertex]:
+                    degrees[neighbour]-=1
+                    if degrees[neighbour]==1:
+                        sources.append(neighbour)
+        return sources
+            
+
+        
