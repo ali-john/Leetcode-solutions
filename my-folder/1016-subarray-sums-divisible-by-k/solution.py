16990a6a3ff110365c93d1e69c8e2c650c1ebe79
Ali John <63426759+ali-john@users.noreply.github.com>
2024-09-17 19:41:04 +0000

    [LeetCode Sync] Runtime - 237 ms (36.45%), Memory - 21.5 MB (68.26%)

---
+++ my-folder/1016-subarray-sums-divisible-by-k/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def subarraysDivByK(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+        table = collections.Counter()
+        table[0] = 1
+        cumm = 0
+        total = 0
+        for i in range(n):
+            cumm+=nums[i]
+
+            if cumm%k in table:
+                total+= table[cumm%k]
+            
+            table[cumm%k]+=1
+        
+        return total
