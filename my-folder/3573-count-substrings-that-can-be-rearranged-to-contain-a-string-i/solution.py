f88d6ac2b0c310408357a1509b3119af0dc5c3af
Ali John <63426759+ali-john@users.noreply.github.com>
2024-09-23 22:35:27 +0000

    [LeetCode Sync] Runtime - 285 ms (58.14%), Memory - 17.5 MB (37.94%)

---
+++ my-folder/3573-count-substrings-that-can-be-rearranged-to-contain-a-string-i/solution.py
@@ -0,0 +1,37 @@
+class Solution:
+    def validSubstringCount(self, word1: str, word2: str) -> int:
+        n = len(word1)
+        k = len(word2)
+        word2_c= Counter(word2)
+        word1_c = Counter()
+        total = 0
+        start = 0
+
+        for i in range(n):
+            curr = word1[i]
+
+            if word2_c[curr]>0:
+                if word1_c[curr]<word2_c[curr]:
+                    k-=1
+            
+            word1_c[curr]+=1
+
+            while k==0:
+                total+=n-i
+                pre = word1[start]
+                word1_c[pre]-=1
+                if word2_c[pre]>0 and word1_c[pre]<word2_c[pre]:
+                    k+=1
+                start+=1
+        return total
+
+        
+
+
+
+
+            
+
+
+
+
