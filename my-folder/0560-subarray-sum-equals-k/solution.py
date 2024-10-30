21748fbd5db825deb25192c480d452da4f55978d
Ali John <63426759+ali-john@users.noreply.github.com>
2024-10-30 00:18:10 +0000

    [LeetCode Sync] Runtime - 61 ms (5.33%), Memory - 19.4 MB (64.63%)

--- my-folder/0560-subarray-sum-equals-k/solution.py
+++ my-folder/0560-subarray-sum-equals-k/solution.py
@@ -1,18 +1,16 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        n = len(nums)
-        table = collections.Counter()
-        table[0] = 1
-        cumm = 0
-        total = 0
-        for i in range(n):
-            cumm+=nums[i]
+        table = Counter()
 
-            if cumm - k in table:
-                total+= table[cumm-k]
-            
-            table[cumm]+=1
-        
+        table[0] = 1
+        cum = 0
+        total = 0
+        for i,num in enumerate(nums):
+            cum+=num
+
+            if cum-k in table:
+                total+=(table[cum-k])
+            table[cum]+=1
         return total
-            
+
 
