62fe7e25dabe96597a056ee31dd3bc26041bbdab
Ali John <63426759+ali-john@users.noreply.github.com>
2024-10-30 01:20:36 +0000

    [LeetCode Sync] Runtime - 38 ms (22.95%), Memory - 17.6 MB (65.11%)

--- my-folder/0966-binary-subarrays-with-sum/solution.py
+++ my-folder/0966-binary-subarrays-with-sum/solution.py
@@ -1,16 +1,27 @@
 class Solution:
     def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
-        n = len(nums)
-        table = collections.Counter()
-        table[0] = 1
-        cumm = 0
-        total = 0
-        for i in range(n):
-            cumm+=nums[i]
-
-            if cumm - goal in table:
-                total+= table[cumm-goal]
-            
-            table[cumm]+=1
         
-        return total
+        # Compute the number of subarrays with sum exactly equal to the goal by subtracting the count of 
+        # subarrays with sum ≤ goal - 1 from the count of subarrays with sum ≤ goal.
+
+        def getsum(goal):
+            n = len(nums)
+            left = 0
+            total = 0
+            total_sum = 0
+
+            for right in range(n):
+                total_sum+=nums[right]
+
+                while total_sum>goal and left<=right:
+                    total_sum-=nums[left]
+                    left+=1
+                
+                total+=(right-left+1)
+            return total
+        
+        return getsum(goal) - getsum(goal-1) 
+
+    
+
+
