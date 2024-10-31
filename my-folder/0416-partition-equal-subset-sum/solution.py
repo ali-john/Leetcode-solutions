9a522618d6aa51b0381a8e4d55523bbde215799a
Ali John <63426759+ali-john@users.noreply.github.com>
2024-10-31 21:44:34 +0000

    [LeetCode Sync] Runtime - 2492 ms (8.47%), Memory - 49 MB (25.11%)

--- my-folder/0416-partition-equal-subset-sum/solution.py
+++ my-folder/0416-partition-equal-subset-sum/solution.py
@@ -2,23 +2,19 @@ class Solution:
     def canPartition(self, nums: List[int]) -> bool:
         n = len(nums)
         total = sum(nums)
-        dp = [[-1 for _ in range(total+1)] for _ in range(n+1)]
+        dp = [[False for _ in range(total+1)] for _ in range(n+1)]
         def recur(nums,n,total):
-            if total==0:
-                return True
-            if n==0:
-                return False
-            
-            if dp[n][total]!=-1:
-                return dp[n][total]
-            
-            if nums[n-1]>total:
-                dp[n][total] = recur(nums,n-1,total)
-            else:
-                dp[n][total] = (recur(nums,n-1,total-nums[n-1])) or (recur(nums,n-1,total))
-            return dp[n][total]
-            
+            for i in range(n+1):
+                dp[i][0] = True
 
+            for i in range(1,n+1):
+                for j in range(1,total+1):
+                    if nums[i-1]>j:
+                        dp[i][j] = dp[i-1][j]
+                    if nums[i-1]<=total:
+                        dp[i][j] = dp[i-1][j - nums[i-1]] or dp[i-1][j]
+            return dp[n][total]
+        
         if total%2!=0:
             return False
         else:
