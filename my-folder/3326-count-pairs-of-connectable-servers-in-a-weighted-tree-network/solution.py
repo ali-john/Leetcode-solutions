3884adbda4af782f68240c5bba65544bce9af008
Ali John <63426759+ali-john@users.noreply.github.com>
2024-11-12 01:33:07 +0000

    [LeetCode Sync] Runtime - 1012 ms (79.41%), Memory - 18.7 MB (50.30%)

--- my-folder/3326-count-pairs-of-connectable-servers-in-a-weighted-tree-network/solution.py
+++ my-folder/3326-count-pairs-of-connectable-servers-in-a-weighted-tree-network/solution.py
@@ -1,52 +1,43 @@
 class Solution:
     def countPairsOfConnectableServers(self, edges: List[List[int]], signalSpeed: int) -> List[int]:
-        n = len(edges)+1
-        degree = [0]*n
-        graph = defaultdict(list[list])
+        n = len(edges)
 
-        for node1, node2, w in edges: # build graph
-            graph[node1].append([node2,w])
-            graph[node2].append([node1,w])
-            degree[node1]+=1
-            degree[node2]+=1
+        graph = {i:[] for i in range(n+1)}
         
+        for node1,node2,w in edges:
+            graph[node1].append((node2,w))
+            graph[node2].append((node1,w))
         
-        def dfs(node,parent, s):
-            
+
+        def dfs(node,parent,s):
             ans = 0
             if s%signalSpeed==0:
                 ans+=1
             
-            for next_node,weight in graph[node]:
-                if next_node==parent:
-                    continue
-                ans+= dfs(next_node,node,s+weight)
-            
+            for child,weight in graph[node]:
+                if child!=parent:
+                    ans+=dfs(child,node,s+weight)
             return ans
 
-
-        to_process = [1]*n
-        for i in range(n):
-            if degree[i]==1: # leaf node, do not have connections
-                to_process[i]=0
-        
-        output = [0]*n
-        for i in range(n):
-            if to_process[i]==1:
+        output = [0]*(n+1)
+        for i in range(n+1):
+            if len(graph[i])>1:
                 res = []
                 for next_node,weight in graph[i]:
                     res.append(dfs(next_node,i, weight))
+                print(f'i: {i}, res= {res}')
                 total = sum(res)
                 ans = 0
                 for curr in res:
-                    ans+= (total-curr )*curr
+                    ans+= (total-curr )*curr # here calculate how many combination we can form from these connected server. lets say [2,3,4] servers are connected then we are actually
+                    # calculating pairs like (2*7)+ (3*6) + (4*5) and we counted duplicates so //2
                 output[i] = ans//2
             else:
                 continue
         return output
-           
-
                 
-
-
-
+        
+        
+            
+            
+            
