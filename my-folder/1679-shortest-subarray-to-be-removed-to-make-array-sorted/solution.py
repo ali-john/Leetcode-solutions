c6ca9012907fc2f68b2ec9c7ed24ba0f0449140e
Ali John <63426759+ali-john@users.noreply.github.com>
2024-11-15 22:18:32 +0000

    [LeetCode Sync] Runtime - 15 ms (32.45%), Memory - 30.2 MB (87.74%)

--- my-folder/1679-shortest-subarray-to-be-removed-to-make-array-sorted/solution.py
+++ my-folder/1679-shortest-subarray-to-be-removed-to-make-array-sorted/solution.py
@@ -1,36 +1,27 @@
 class Solution:
     def findLengthOfShortestSubarray(self, arr: List[int]) -> int:
-        l = 0
-        r = len(arr)-1
+        n = len(arr)
+        left = 0
+        right = n-1
 
-        while l<len(arr)-1 and arr[l]<=arr[l+1]:
-            l+=1
+        while left<n-1 and (arr[left]<=arr[left+1]):
+            left+=1
         
-        if l == len(arr)-1:
+        if left==n-1:
             return 0
-        while r>0 and arr[r]>=arr[r-1]:
-            r-=1
         
-        minRemoval = min(len(arr)-l-1,r)
+        while right>0 and arr[right]>=arr[right-1]:
+            right-=1
+        
+        min_length = min(right,n-left-1)
+        
         i=0
-        j = r
-        while i<=l and j<len(arr):
+        j = right
+        # remove middle portion
+        while i<=left and j<len(arr):
             if arr[i]<=arr[j]:
-                minRemoval = min(j-i-1,minRemoval)
+                min_length = min(j-i-1,min_length)
                 i+=1
             else:
                 j+=1
-        return minRemoval
-
-        
-
-        
-
-        
-
-
-
-
-
-
-        
+        return min_length
