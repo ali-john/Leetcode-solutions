269b59a5f1ab3f5e21f4c148ba260d5c9f898b64
Ali John <63426759+ali-john@users.noreply.github.com>
2024-11-18 20:29:00 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 16.6 MB (44.84%)

--- my-folder/0198-house-robber/solution.py
+++ my-folder/0198-house-robber/solution.py
@@ -1,11 +1,15 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        rob1, rob2 = 0,0
+        n = len(nums)
+        if n==1:
+            return nums[0]
+        if n==2:
+            return max(nums[0],nums[1])
+            
+        dp = [-1 for _ in range(n)]
+        dp[0] = nums[0]
+        dp[1] = max(nums[0],nums[1])
+        for i in range(2,n):
+            dp[i] = max(dp[i-1],dp[i-2]+nums[i])
+        return dp[n-1]
 
-        for n in nums:
-            temp = max(n+rob1,rob2)
-            rob1 = rob2
-            rob2=temp
-        return rob2
-
-        
