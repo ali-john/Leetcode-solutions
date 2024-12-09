ad0ec55880e393091615b8a9f8b2023c0f6c0c05
Ali John <63426759+ali-john@users.noreply.github.com>
2024-12-09 19:38:34 +0000

    [LeetCode Sync] Runtime - 7 ms (87.28%), Memory - 17.2 MB (86.09%)

--- my-folder/3681-maximum-area-rectangle-with-point-constraints-i/solution.py
+++ my-folder/3681-maximum-area-rectangle-with-point-constraints-i/solution.py
@@ -1,33 +1,45 @@
 class Solution:
     def maxRectangleArea(self, points: List[List[int]]) -> int:
-        point_set = set(tuple(p) for p in points)
+        points_set = set(map(tuple,points))
+        n = len(points)
         max_area = -1
 
-        def has_points_inside(x_min, x_max, y_min, y_max, corners):
-            for px, py in points:
-                if (px, py) in corners:
+
+        def has_point_inside(x_min,x_max,y_min,y_max,corners):
+            for point in points:
+                if tuple(point) in corners:
                     continue
-                if x_min < px < x_max and y_min < py < y_max:
-                    return True
-                if (px == x_min or px == x_max) and y_min <= py <= y_max:
-                    return True  
-                if (py == y_min or py == y_max) and x_min <= px <= x_max:
-                    return True  
+                
+                else:
+                    x,y = point[0],point[1]
+                    if x_min < x < x_max and y_min < y < y_max:
+                        return True
+                    if (x == x_min or x==x_max) and (y_min <= y <= y_max):
+                        return True
+                    if (x_min <= x <= x_max) and (y == y_min or y == y_max):
+                        return True
             return False
-    
-        n = len(points)
+
+
         for i in range(n):
-            for j in range(i + 1, n):
-                x1, y1 = points[i]
-                x2, y2 = points[j]
-    
-                if x1 != x2 and y1 != y2:
-                    corners = {(x1, y1), (x1, y2), (x2, y1), (x2, y2)}
-                    if (x1, y2) in point_set and (x2, y1) in point_set:
-                        if not has_points_inside(
-                            min(x1, x2), max(x1, x2), min(y1, y2), max(y1, y2), corners
-                        ):
-                            area = abs(x2 - x1) * abs(y2 - y1)
-                            max_area = max(max_area, area)
-    
-        return max_area if max_area != -1 else -1
+            for j in range(i+1,n):
+                x1,y1 = points[i][0], points[i][1]
+                x2,y2 = points[j][0],points[j][1]
+
+                if x1!=x2 and y1!=y2:
+                    corners = {(x1,y1),(x2,y2),(x2,y1),(x1,y2)}
+                    p1 = (x2,y1)
+                    p2 = (x1,y2)
+
+                    if p1 in points_set and p2 in points_set:
+                        if not has_point_inside(min(x1,x2),max(x1,x2),min(y1,y2),max(y1,y2),corners):
+                            area = abs(x1-x2)*abs(y1-y2)
+                            max_area = max(max_area,area)
+        if max_area!=-1: 
+            return max_area
+        else:
+            return -1
+
+
+
+
