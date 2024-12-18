aab23367d70f23db2cbdf322823d3dfe724ad279
Ali John <63426759+ali-john@users.noreply.github.com>
2024-12-18 19:16:15 +0000

    [LeetCode Sync] Runtime - 51 ms (80.56%), Memory - 18.1 MB (26.25%)

---
+++ my-folder/0387-first-unique-character-in-a-string/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        counter = Counter(s)
+
+        for i in range(len(s)):
+            if counter[s[i]]==1:
+                return i
+        return -1
