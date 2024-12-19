f5df83e963182373b394f2b2d17e2e4dabf1f588
Ali John <63426759+ali-john@users.noreply.github.com>
2024-12-19 01:10:52 +0000

    [LeetCode Sync] Runtime - 514 ms (23.32%), Memory - 66.5 MB (59.45%)

--- my-folder/3071-drop-duplicate-rows/solution.py
+++ my-folder/3071-drop-duplicate-rows/solution.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
 def dropDuplicateEmails(customers: pd.DataFrame) -> pd.DataFrame:
-    customers.drop_duplicates(subset = ['email'], inplace = True)
+    customers.drop_duplicates(subset = ['email'],inplace=True)
     return customers
