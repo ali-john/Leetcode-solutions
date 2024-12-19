2de68a7d6c86dd515d7b8ddf420d44ce3f7cb3a8
Ali John <63426759+ali-john@users.noreply.github.com>
2024-12-19 01:26:38 +0000

    [LeetCode Sync] Runtime - 443 ms (45.01%), Memory - 66.8 MB (41.67%)

--- my-folder/3063-method-chaining/solution.py
+++ my-folder/3063-method-chaining/solution.py
@@ -2,6 +2,6 @@ import pandas as pd
 
 def findHeavyAnimals(animals: pd.DataFrame) -> pd.DataFrame:
 
-    heavy_animals = animals[animals['weight'] >100]
+    heavy_animals = animals[animals['weight']>100]
     sorted_animals = heavy_animals.sort_values(by='weight',ascending=False)
     return sorted_animals[['name']]
