eeeb03418980f840d7286acad717b58c871e87bd
Ali John <63426759+ali-john@users.noreply.github.com>
2024-12-19 01:11:44 +0000

    [LeetCode Sync] Runtime - 376 ms (61.16%), Memory - 67.1 MB (27.67%)

--- my-folder/3075-drop-missing-data/solution.py
+++ my-folder/3075-drop-missing-data/solution.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
 def dropMissingData(students: pd.DataFrame) -> pd.DataFrame:
-    students.dropna(subset = ['name'],inplace = True)
+    students.dropna(subset=['name'],inplace=True)
     return students
