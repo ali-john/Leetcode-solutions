9389a582320a4b0712908370e6eff6e32a30bfdd
Ali John <63426759+ali-john@users.noreply.github.com>
2024-12-19 01:20:55 +0000

    [LeetCode Sync] Runtime - 562 ms (25.00%), Memory - 67.9 MB (23.41%)

--- my-folder/3072-reshape-data-pivot/solution.py
+++ my-folder/3072-reshape-data-pivot/solution.py
@@ -1,5 +1,4 @@
 import pandas as pd
 
 def pivotTable(weather: pd.DataFrame) -> pd.DataFrame:
-    return pd.pivot_table(weather,index = 'month',columns= "city",values="temperature")
-    #return weather
+    return pd.pivot_table(weather,index='month',columns='city',values='temperature')
