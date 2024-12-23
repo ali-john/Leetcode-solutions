546376fa0bdc0c195f13dfefc9667d5450fde75c
Ali John <63426759+ali-john@users.noreply.github.com>
2024-12-23 22:15:19 +0000

    [LeetCode Sync] Runtime - 3 ms (90.92%), Memory - 19 MB (45.92%)

---
+++ my-folder/0323-number-of-connected-components-in-an-undirected-graph/solution.py
@@ -0,0 +1,41 @@
+class UnionFind:
+    def __init__(self,n):
+        self.root = [i for i in range(n)]
+        self.rank = [1]*n
+        
+    def find(self,value):
+        if self.root[value]==value:
+            return value
+        self.root[value] = self.find(self.root[value])
+        return self.root[value]
+
+    def union(self,u,v):
+        u_parent = self.find(u)
+        v_parent = self.find(v)
+        if u_parent!=v_parent:
+            if self.rank[u_parent] > self.rank[v_parent]:
+                self.root[v_parent] = u_parent
+            elif self.rank[v_parent] > self.rank[u_parent]:
+                self.root[u_parent] = v_parent
+            else:
+                self.root[u_parent] = v_parent
+                self.rank[v_parent]+=1
+    
+    def is_connected(self,u,v):
+        return self.find(u)==self.find(v)
+
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        uf = UnionFind(n)
+        for u,v in edges:
+            uf.union(u,v)
+        
+        components = 0
+        for i in range(n):
+            if uf.find(i)==i:
+                components+=1
+        return components
+        
+        
+        
+        
