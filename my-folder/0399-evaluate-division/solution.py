cfe8f3dd5a187083742d2a0cb5032c5c31c63c7c
Ali John <63426759+ali-john@users.noreply.github.com>
2024-12-24 01:11:33 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 18.1 MB (13.57%)

--- my-folder/0399-evaluate-division/solution.py
+++ my-folder/0399-evaluate-division/solution.py
@@ -1,63 +1,37 @@
 class Solution:
-    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
-        graph = {}
-        output = [-1 for _ in range(len(queries))]
-        for i in range(len(equations)):
-            x1,x2 = equations[i]
-            cost = values[i]
-            if x1 not in graph:
-                graph[x1] = []
-            graph[x1].append((x2,cost))
-            if x2 not in graph:
-                graph[x2] = []
-            graph[x2].append((x1,1/cost))
+    def calcEquation(self, eq: List[List[str]], val: List[float], q: List[List[str]]) -> List[float]:
+    
+        n = len(eq)
+        graph = defaultdict(list)
+        for (a,b),cost in zip(eq,val):
+            graph[a].append((b,cost))
+            graph[b].append((a,1/cost))
 
-        def bfs(s,d,graph):
-            q = []
-            q.append((s,1))
-            visited = set()
-            total_cost = 1
+        print(graph)
+        def dfs(start,end,path_cost,visited):
+            
+            if start==end:
+                return path_cost
+            visited.add(start)
+            for nei,cost in graph[start]:
+                if nei not in visited:
+                    value = dfs(nei,end,path_cost*cost,visited)
+                    if value is not None:
+                        return value
+            
+        output = [-1]*len(q)
 
-            while q:
-                s,cost = q.pop(0)
-                visited.add(s)
-                nei = graph.get(s)
-                
-                for next_n,val in nei:
-                    if next_n == d:
-                        total_cost = cost* val
-                        return total_cost
-                    else:
-                        qvals = [x for (x,y) in q]
-                        if next_n not in visited and next_n not in qvals:
-                            new_val = val*cost
-                            q.append((next_n,new_val))
-            return -1 # not found a path
-        index=-1
-        keys = graph.keys()
-        for q1,q2 in queries:
-            index+=1
-            if q1 in keys and q2 in keys:
-                output[index] = bfs(q1,q2,graph)
+        for i,(start,end) in enumerate(q):
+            if start==end and start in graph:
+                output[i] = 1
+            elif start not in graph or end not in graph:
+                output[i] = -1
+            else:
+                out = dfs(start,end,1,set())
+                if out is not None:
+                    output[i] = out
         return output
-
-            
-
-
-                    
-
-
-
-
-
-
-
-
-        
-        
             
 
 
 
-
-        
