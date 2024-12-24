fe30363edef6f362624bce78d4a8baa8b119a852
Ali John <63426759+ali-john@users.noreply.github.com>
2024-12-24 21:28:53 +0000

    [LeetCode Sync] Runtime - 37 ms (83.60%), Memory - 18.2 MB (17.24%)

--- my-folder/0133-clone-graph/solution.py
+++ my-folder/0133-clone-graph/solution.py
@@ -9,67 +9,35 @@ class Node:
 from typing import Optional
 class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
-        if not node:
-            return
-        visited = set()
-    
-        nodes_created = {}
-        def bfs(node):
-            q = []
-            q.append(node)
+        
+        graph = defaultdict(list)
+        new_graph = defaultdict(list)
+        
+        def dfs(curr):
+            if not curr:
+                return curr
+            if curr in new_graph:
+                return new_graph[curr]
             
-            while q:
-                n = q.pop(0)
-                nei = n.neighbors
-                val = n.val
-                keys = nodes_created.keys()
-                visited.add(val)
-                if val not in keys:
-                    new_node = Node(val = val)
-                    nodes_created[val] = new_node
-                    for i in nei:
-                        if i.val not in keys:
-                            nn_nei  = Node(val = i.val)
-                            nodes_created[i.val] = nn_nei
-                            new_node.neighbors.append(nn_nei)
-                            q.append(i)
-                        else:
-                            nn_nei = nodes_created.get(i.val)
-                            new_node.neighbors.append(nn_nei)
-                            qvals = [x.val for x in q]
-                            if not(i.val in visited or i.val in qvals):
-                                q.append(i)
-                else:
-                    new_node = nodes_created.get(val)
-                    for i in nei:
-                        if i.val not in keys:
-                            nn_nei  = Node(val = i.val)
-                            nodes_created[i.val] = nn_nei
-                            new_node.neighbors.append(nn_nei)
-                            q.append(i)
-                        else:
-                            nn_nei = nodes_created.get(i.val)
-                            new_node.neighbors.append(nn_nei)
-                            qvals = [x.val for x in q]
-                            if not (i.val in visited or i.val in qvals):
-                                q.append(i)
-                
+            new_node = Node(curr.val,[])
+            new_graph[curr] = new_node
+
+            if curr.neighbors:
+                new_node.neighbors = [dfs(n) for n in curr.neighbors]
+            
+            return new_node
         
-        bfs(node) 
-        return nodes_created.get(1)
-
-
-                
-
-                        
+        return dfs(node)
 
 
 
 
+            
 
 
 
 
-        
+            
+            
+                    
 
-        
