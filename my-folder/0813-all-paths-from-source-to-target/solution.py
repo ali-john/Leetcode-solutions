290f04cc12e83e9b3fda1211fd8f9412153f0bac
Ali John <63426759+ali-john@users.noreply.github.com>
2024-12-27 00:42:54 +0000

    [LeetCode Sync] Runtime - 7 ms (42.17%), Memory - 19.2 MB (26.84%)

--- my-folder/0813-all-paths-from-source-to-target/solution.py
+++ my-folder/0813-all-paths-from-source-to-target/solution.py
@@ -6,16 +6,30 @@ class Solution:
         for i,l in enumerate(g):
             graph[i] = l
         output = []
-
-
-        def dfs(curr,path):
-            if curr==n-1:
-                output.append(path[:])
-            
-            for nei in graph[curr]:
-                path.append(nei)
-                dfs(nei,path)
-                path.pop()
-
-        dfs(0,[0])
+        
+        q = [[0]]
+        while q:
+            curr_path = q.pop(0)
+            curr_node = curr_path[-1]
+            for nei in graph[curr_node]:
+                temp_path = curr_path.copy()
+                temp_path.append(nei)
+                if nei == len(graph)-1:
+                    output.append(temp_path)
+                else:
+                    q.append(temp_path)
         return output
+                
+        
+
+        # def dfs(curr,path):
+        #     if curr==n-1:
+        #         output.append(path[:])
+            
+        #     for nei in graph[curr]:
+        #         path.append(nei)
+        #         dfs(nei,path)
+        #         path.pop()
+
+        # dfs(0,[0])
+        # return output
