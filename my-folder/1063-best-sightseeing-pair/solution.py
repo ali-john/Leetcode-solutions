d97d8452610225ef5ca192b88f5dde04a5ef4c82
Ali John <63426759+ali-john@users.noreply.github.com>
2024-12-28 01:18:09 +0000

    [LeetCode Sync] Runtime - 91 ms (10.91%), Memory - 23.1 MB (77.76%)

--- my-folder/1063-best-sightseeing-pair/solution.py
+++ my-folder/1063-best-sightseeing-pair/solution.py
@@ -2,16 +2,15 @@ class Solution:
     def maxScoreSightseeingPair(self, values: List[int]) -> int:
         n = len(values)
 
-        left_scores = [0]*n
-        left_scores[0] = values[0]
+        max_left_scores = [0]*n
+        max_left_scores[0] = values[0]
 
         max_score = 0
 
         for i in range(1,n):
-            curr_left_score = left_scores[i-1]
+            left = max_left_scores[i-1]
+            curr_val = values[i] - i
+            max_score = max(max_score,left+curr_val)
 
-            curr_score = (values[i] - i) + curr_left_score
-            max_score = max(max_score,curr_score)
-            curr_left_score = max(curr_left_score,values[i]+i)
-            left_scores[i] = curr_left_score
+            max_left_scores[i] = max(max_left_scores[i-1],values[i]+i)
         return max_score
