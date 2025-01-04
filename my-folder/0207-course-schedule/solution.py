fb1f124253c62159eb40d3f8fb289928d3c7471d
Ali John <63426759+ali-john@users.noreply.github.com>
2025-01-04 02:10:13 +0000

    [LeetCode Sync] Runtime - 1 ms (93.28%), Memory - 19.2 MB (32.22%)

--- my-folder/0207-course-schedule/solution.py
+++ my-folder/0207-course-schedule/solution.py
@@ -1,37 +1,27 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        graph = defaultdict(list)
         in_degree = [0]*numCourses
-        for c1, c2 in prerequisites:
-            graph[c1].append(c2)
-            in_degree[c2]+=1
+        graph = defaultdict(list)
+        
+        for course,pre in prerequisites:
+            graph[pre].append(course)
+            in_degree[course]+=1
         
         sources = []
-        for i in range(len(in_degree)):
-            if in_degree[i]==0:
+        for i,degree in enumerate(in_degree):
+            if degree==0:
                 sources.append(i)
+        
         schedule = []
+
         while sources:
-            vertex = sources.pop(0)
-            schedule.append(vertex)
-            for child in graph[vertex]:
+            node = sources.pop(0)
+            schedule.append(node)
+
+            for child in graph[node]:
                 in_degree[child]-=1
                 if in_degree[child]==0:
                     sources.append(child)
-    
         return len(schedule)==numCourses
 
 
-
-
-
-            
-
-            
-
-            
-
-
-
-
-        
