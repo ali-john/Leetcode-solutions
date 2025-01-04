34833a5a75bef7ca1e9c78ab3b4cd29e6694bf3f
Ali John <63426759+ali-john@users.noreply.github.com>
2025-01-04 02:17:27 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.1 MB (42.02%)

--- my-folder/0210-course-schedule-ii/solution.py
+++ my-folder/0210-course-schedule-ii/solution.py
@@ -1,32 +1,25 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        graph = defaultdict(list)
         in_degree = [0]*numCourses
-        for c1, c2 in prerequisites:
-            graph[c2].append(c1)
-            in_degree[c1]+=1
+        graph = defaultdict(list)
+
+        for course,pre in prerequisites:
+            graph[pre].append(course)
+            in_degree[course]+=1
         
         sources = []
-        for i in range(len(in_degree)):
-            if in_degree[i]==0:
-                sources.append(i)
+
+        for i, degree in enumerate(in_degree):
+            if degree==0: sources.append(i)
+        
         schedule = []
         while sources:
-            vertex = sources.pop(0)
-            schedule.append(vertex)
-            for child in graph[vertex]:
-                in_degree[child]-=1
-                if in_degree[child]==0:
-                    sources.append(child)
-    
-        if len(schedule)==numCourses:
-            return schedule
-        else:
-            return []
-
-
+            node = sources.pop(0)
+            schedule.append(node)
             
+            for child in graph[node]:
+                in_degree[child]-=1
+                if in_degree[child]==0: sources.append(child)
+        return schedule if len(schedule)==numCourses else []
 
 
-
-        
