73591860f1ae569ea79ad22f7c95bb0b16fa09a2
Ali John <63426759+ali-john@users.noreply.github.com>
2025-01-04 21:23:33 +0000

    [LeetCode Sync] Runtime - 7 ms (61.26%), Memory - 21.5 MB (54.21%)

--- my-folder/0130-surrounded-regions/solution.py
+++ my-folder/0130-surrounded-regions/solution.py
@@ -3,59 +3,29 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-        if not board:
-            return 
-        rows,cols = len(board), len(board[0])
-        visited = set()
+        rows, cols = len(board), len(board[0])
 
-        
-        def bfs(r,c):
-            q = []
-            q.append((r,c))
-            visited.add((r,c))
-            board[r][c] = "T"
-            directions = [[1,0],[-1,0],[0,1],[0,-1]]
-            while q:
-                r,c = q.pop(0)
-                for dx,dy in directions:
-                    row, col = r+dx, c+dy
-                    if row in range(rows) and col in range(cols) and board[row][col]=="O" and (row,col) not in visited:
-                        q.append((row,col))
-                        visited.add((row,col))
-                        board[row][col] = "T"
-        for r in range(rows):
-            for c in range(cols):
-                if r==0 or r==rows-1 or c==0 or c==cols-1: # boundary condition
-                    if board[r][c] == "O":
-                        bfs(r,c) # mark all connected components as T
-
-        for r in range(rows):
-            for c in range(cols):
-                if board[r][c] == "O":
-                    board[r][c] = "X"
+        def dfs(r,c):
+            if (r<0 or r>=rows or c<0 or c>=cols or board[r][c]!='O'):
+                return
+            board[r][c] = 'T'
+            dfs(r+1,c)
+            dfs(r-1,c)
+            dfs(r,c+1)
+            dfs(r,c-1)
         
         for r in range(rows):
             for c in range(cols):
-                if board[r][c] == "T":
-                    board[r][c] = "O"        
-
-                
-        return
-
-
-
+                if ( board[r][c]== 'O' and  (r in [0,rows -1] or c in [0, cols-1]) ):
+                    dfs(r,c)
+        # replace all O with X
+        for i in range(rows):
+            for j in range(cols):
+                if board[i][j]=='O':
+                    board[i][j] = 'X'
         
-
-        
-
-        
-            
-    
-        
-        
-        
-
-
-
-
+        for i in range(rows):
+            for j in range(cols):
+                if board[i][j] == 'T':
+                    board[i][j] = 'O'
         
