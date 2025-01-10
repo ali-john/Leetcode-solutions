227d492afed83a231ad930f4776730c68fa4d442
Ali John <63426759+ali-john@users.noreply.github.com>
2025-01-10 00:05:44 +0000

    [LeetCode Sync] Runtime - 269 ms (31.84%), Memory - 20.1 MB (52.60%)

--- my-folder/0200-number-of-islands/solution.py
+++ my-folder/0200-number-of-islands/solution.py
@@ -1,38 +1,26 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid:
-            return 0
+        rows, cols = len(grid), len(grid[0])
 
-        rows,cols = len(grid), len(grid[0])
-        visited = set()
-        islands = 0
-    
-        def bfs(r,c):
-            q = []
-            q.append((r,c))
-            visited.add((r,c))
-            while q:
-                row,col = q.pop(0)
-                directions = [[1,0],[-1,0],[0,1],[0,-1]]
-                for dx,dy in directions:
-                    r,c = row+dx, col+dy
-                    if (r in range(rows) and c in range(cols) and grid[r][c]=="1" and (r,c) not in visited):
-                        visited.add((r,c))
-                        q.append((r,c))
-        
-        for i in range(rows):
-            for j in range(cols):
-                if grid[i][j]=="1" and (i,j) not in visited:
-                    islands+=1
-                    visited.add((i,j))
-                    bfs(i,j)
-        return islands
+        def dfs(i,j,visited):
+            if i<0 or i>=rows or j<0 or j>=cols or grid[i][j]!='1':
+                return
+            
+            grid[i][j] = 'T'
+            visited.add((i,j))
+            directions = [[1,0],[-1,0],[0,-1],[0,1]]
+            for direction in directions:
+                new_i = i + direction[0]
+                new_j = j + direction[1]
+                if (new_i,new_j) not in visited:
+                    dfs(new_i,new_j,visited)
+            return 
 
-
-
-
-
-
-        
-
+        count = 0
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c]=='1': 
+                    dfs(r,c,set())
+                    count+=1
+        return count
         
