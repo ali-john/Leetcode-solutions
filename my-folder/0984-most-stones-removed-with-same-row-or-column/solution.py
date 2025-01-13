b8a3abff4f31e7cc8ed1eafa64e46c0da8f9c13c
Ali John <63426759+ali-john@users.noreply.github.com>
2025-01-13 22:10:00 +0000

    [LeetCode Sync] Runtime - 623 ms (21.58%), Memory - 18.4 MB (46.25%)

--- my-folder/0984-most-stones-removed-with-same-row-or-column/solution.py
+++ my-folder/0984-most-stones-removed-with-same-row-or-column/solution.py
@@ -1,24 +1,44 @@
 class Solution:
     def removeStones(self, stones: List[List[int]]) -> int:
         n = len(stones)
-        graph = {i:[] for i in range(n)}
+        uf = UnionFind(n)
+
+        components = n
         for i in range(n):
             for j in range(i+1,n):
-                if stones[i][0]==stones[j][0] or stones[i][1]==stones[j][1]: #any common row/col
-                    graph[i].append(j)
-                    graph[j].append(i)
-        visited = [False]*n
-        def dfs(node):
-            visited[node] = True
-            for neighbour in graph[node]:
-                if not visited[neighbour]:
-                    dfs(neighbour)
-        components = 0
-        for i in range(n):
-            if not visited[i]:
-                dfs(i)
-                components+=1
-        return n-components
+                if stones[i][0] == stones[j][0] or stones[i][1] == stones[j][1]:
+                    uf.union(i,j)
+
+        return n-uf.n
+
+
+class UnionFind:
+   def __init__(self,n):
+       self.rank = [1]*n
+       self.root = [i for i in range(n)]
+       self.n = n
+  
+   def find(self,value):
+       if self.root[value]==value:
+           return value
+       self.root[value] = self.find(self.root[value])
+       return self.root[value]
+  
+   def union(self,u,v):
+       u_parent = self.find(u)
+       v_parent = self.find(v)
+       if u_parent!=v_parent:
+           self.n-=1
+           if self.rank[u_parent] > self.rank[v_parent]:
+               self.root[v_parent] = u_parent
+           elif self.rank[v_parent] > self.rank[u_parent]:
+               self.root[u_parent] = v_parent
+           else:
+               self.root[u_parent] = v_parent
+               self.rank[v_parent]+=1
+  
+   def is_connected(self,u,v):
+       return self.find(u)==self.find(v)
 
 
 
