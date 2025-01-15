d8bef6ffc786be03be0e076c718428876b865695
Ali John <63426759+ali-john@users.noreply.github.com>
2025-01-15 20:14:38 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.8 MB (26.53%)

--- my-folder/0070-climbing-stairs/solution.py
+++ my-folder/0070-climbing-stairs/solution.py
@@ -1,11 +1,11 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        @cache
-        def solve(rem):
-            if rem==0:
-                return 1
-            if rem<0:
-                return 0
-            
-            return solve(rem-1)+ solve(rem-2)
-        return solve(n)
+        memo = defaultdict(int)
+
+        def dp(i):
+            if i==1:return 1
+            if i==2: return 2
+            if i in memo: return memo[i]
+            memo[i] = dp(i-1) + dp(i-2)
+            return memo[i]
+        return dp(n)
