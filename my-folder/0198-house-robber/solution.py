3bbf80c67abf54275212236846add945116895e4
Ali John <63426759+ali-john@users.noreply.github.com>
2025-01-15 20:34:07 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 18 MB (7.21%)

--- my-folder/0198-house-robber/solution.py
+++ my-folder/0198-house-robber/solution.py
@@ -1,15 +1,13 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        n = len(nums)
-        if n==1:
-            return nums[0]
-        if n==2:
-            return max(nums[0],nums[1])
-            
-        dp = [-1 for _ in range(n)]
-        dp[0] = nums[0]
-        dp[1] = max(nums[0],nums[1])
-        for i in range(2,n):
-            dp[i] = max(dp[i-1],dp[i-2]+nums[i])
-        return dp[n-1]
+        memo = defaultdict(int)
+
+        def dp(i):
+            if i==0: return nums[0]
+            if i==1: return max(nums[0],nums[1])
+            if i in memo: return memo[i]
+            memo[i] = max(dp(i-1),nums[i]+dp(i-2))
+            return memo[i]
+        
+        return dp(len(nums)-1)
 
