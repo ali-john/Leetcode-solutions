cfc8c22ef13228f316a16330dccef7d54ea5218e
Ali John <63426759+ali-john@users.noreply.github.com>
2025-01-16 20:20:14 +0000

    [LeetCode Sync] Runtime - 3156 ms (5.16%), Memory - 281.9 MB (5.07%)

--- my-folder/1896-maximum-score-from-performing-multiplication-operations/solution.py
+++ my-folder/1896-maximum-score-from-performing-multiplication-operations/solution.py
@@ -2,18 +2,16 @@ class Solution:
     def maximumScore(self, nums: List[int], mul: List[int]) -> int:
         n = len(nums)
         m = len(mul)
-        dp = [[-1 for _ in range(m+1)]  for _ in range(m+1)]
-        def solve(start,i):
-            if i>=len(mul):
-                return 0
-            if dp[start][i]!=-1:
-                return dp[start][i]
-            end = (n - (i-start))-1
-            
-            first = nums[start]*mul[i] + solve(start+1,i+1)
-            second = nums[end]*mul[i] + solve(start,i+1)
+        dp = [[-1 for _ in range(m+1)] for _ in range(n+1) ]
 
-            ans = max(first,second)
-            dp[start][i] = ans
-            return ans
+        def solve(start,i):
+            if i>=m:return 0
+
+            if dp[start][i]!=-1: return dp[start][i]
+            end = (n - (i-start)) - 1
+            from_start = nums[start]*mul[i] + solve(start+1,i+1)
+            from_end = nums[end]*mul[i] + solve(start,i+1)
+            dp[start][i] = max(from_start,from_end)
+            return dp[start][i]
         return solve(0,0)
+        
