f3fa061a55232bd1d9dfe0010bb36544b054cfc3
Ali John <63426759+ali-john@users.noreply.github.com>
2025-01-20 20:34:24 +0000

    [LeetCode Sync] Runtime - 3 ms (73.42%), Memory - 17.8 MB (86.67%)

--- my-folder/0747-min-cost-climbing-stairs/solution.py
+++ my-folder/0747-min-cost-climbing-stairs/solution.py
@@ -2,14 +2,22 @@ class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
         memo = defaultdict(int)
         n = len(cost)
-        def dp(i):
-            if i<=1: return 0
-            if i in memo: return memo[i]
-            one_step = cost[i-1] + dp(i-1)
-            two_step = cost[i-2] + dp(i-2)
-            memo[i] = min(one_step,two_step)
-            return memo[i]
-        return dp(n)
+        # def dp(i):
+        #     if i<=1: return 0
+        #     if i in memo: return memo[i]
+        #     one_step = cost[i-1] + dp(i-1)
+        #     two_step = cost[i-2] + dp(i-2)
+        #     memo[i] = min(one_step,two_step)
+        #     return memo[i]
+        # return dp(n)
+
+        dp = [0]*(n+1)
+        
+        for i in range(2,n+1):
+            one_step = cost[i-1] + dp[i-1]
+            two_step = cost[i-2] + dp[i-2]
+            dp[i] = min(one_step,two_step)
+        return dp[-1]
 
 
 
