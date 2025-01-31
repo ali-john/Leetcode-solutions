c84ee4c002595cfb7991504653ecb4045116665d
Ali John <63426759+ali-john@users.noreply.github.com>
2025-01-31 19:14:53 +0000

    [LeetCode Sync] Runtime - 3 ms (49.94%), Memory - 18.9 MB (51.05%)

--- my-folder/0001-two-sum/solution.py
+++ my-folder/0001-two-sum/solution.py
@@ -2,12 +2,10 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         output = []
         table = defaultdict(int)
-
+        
         for i,num in enumerate(nums):
-            if target-num in table:
+            if target - num in table:
                 output.append(i)
-                output.append(table[target-num])
-                return output
-            
+                output.append(table[target - num])
             table[num] = i
         return output
