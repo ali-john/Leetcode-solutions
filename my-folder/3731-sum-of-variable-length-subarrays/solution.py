9ecd3e0fe34a8847c9f76d8fa9e29ecb1fb12e83
Ali John <63426759+ali-john@users.noreply.github.com>
2025-02-04 20:10:15 +0000

    [LeetCode Sync] Runtime - 18 ms (12.81%), Memory - 17.9 MB (43.74%)

--- my-folder/3731-sum-of-variable-length-subarrays/solution.py
+++ my-folder/3731-sum-of-variable-length-subarrays/solution.py
@@ -1,13 +1,11 @@
 class Solution:
     def subarraySum(self, nums: List[int]) -> int:
         n = len(nums)
-
         ans = 0
+
         for i in range(n):
-            start = max(0,i-nums[i])
-            local = 0
+            start = max(0,i - nums[i])
             for j in range(start,i+1):
-                local+=nums[j]
-            ans+=local
+                ans+=nums[j]
         return ans
 
