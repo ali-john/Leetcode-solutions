601a97d6b6219e5947d3529bb297c04eab25b11f
Ali John <63426759+ali-john@users.noreply.github.com>
2025-02-08 21:05:44 +0000

    [LeetCode Sync] Runtime - 1121 ms (7.66%), Memory - 0.0B (100.00%)

---
+++ my-folder/1734-bank-account-summary-ii/solution.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+
+SELECT name, SUM(amount) as balance
+FROM Users u
+LEFT JOIN Transactions t ON u.account = t.account
+GROUP BY u.account
+HAVING balance >10000
+
