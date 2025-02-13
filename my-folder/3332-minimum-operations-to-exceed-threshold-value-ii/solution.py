362cfa9fa96e70ce7e7c422e2afc37a96ce7b06f
Ali John <63426759+ali-john@users.noreply.github.com>
2025-02-13 03:11:56 +0000

    [LeetCode Sync] Runtime - 272 ms (18.12%), Memory - 37.8 MB (7.25%)

--- my-folder/3332-minimum-operations-to-exceed-threshold-value-ii/solution.py
+++ my-folder/3332-minimum-operations-to-exceed-threshold-value-ii/solution.py
@@ -1,20 +1,22 @@
 class Solution:
     def minOperations(self, nums: List[int], k: int) -> int:
+
+        n = len(nums)
         heap = []
 
         for num in nums:
             heapq.heappush(heap,num)
-
-        ans =0
-        while len(heap)>=2:
-            first = heappop(heap)
-            second = heappop(heap)
-            if first>=k and second>=k:
-                return ans
-            op = min(first,second)*2 + max(first,second)
-            heapq.heappush(heap,op)
-            ans+=1
-
-
-        return ans
         
+        op = 0
+        while len(heap)>=2:
+            n1 = heapq.heappop(heap)
+            n2 = heapq.heappop(heap)
+            if n1>=k and n2>=k:
+                return op
+            
+            op+=1
+            heapq.heappush(heap,min(n1,n2)*2 + max(n1,n2))
+        return op
+        
+        
+
