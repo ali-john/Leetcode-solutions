aa7778c946d299735760720d7058a31e27122d2e
Ali John <63426759+ali-john@users.noreply.github.com>
2025-02-14 20:21:08 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.7 MB (86.04%)

---
+++ my-folder/0031-next-permutation/solution.py
@@ -0,0 +1,22 @@
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+
+        i = n - 2
+        while i>=0 and nums[i]>=nums[i+1]:
+            i-=1
+        if i<0:
+            nums.sort()
+            return 
+        
+        j = n - 1
+        while nums[j]<=nums[i]:
+            j-=1
+        
+        nums[j], nums[i] = nums[i], nums[j]
+        nums[i+1:] = reversed(nums[i+1:])
+        return nums
+        
