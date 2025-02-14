69f357733b4519c5d4b6326d3ed54ae21b41d0f1
Ali John <63426759+ali-john@users.noreply.github.com>
2025-02-14 20:14:37 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.9 MB (16.18%)

---
+++ my-folder/0556-next-greater-element-iii/solution.py
@@ -0,0 +1,20 @@
+class Solution:
+    def nextGreaterElement(self, n: int) -> int:
+        nums = [d for d in str(n)]
+        l = len(nums)
+
+        if l==1: return -1
+        i = l-2
+        while i>=0 and nums[i]>=nums[i+1]:
+            i-=1
+        if i<0: return -1
+
+        j = l - 1
+        while nums[j]<=nums[i]:
+            j-=1
+        nums[j], nums[i] = nums[i], nums[j]
+        nums[i+1:] = reversed(nums[i+1:])
+        ans = int("".join(nums))
+        return ans if ans< 2**31 else -1
+
+
