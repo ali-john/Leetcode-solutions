4cd55fd4085b428bba8bc17db2e0a0ceed527c74
Ali John <63426759+ali-john@users.noreply.github.com>
2025-02-24 16:51:10 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.8 MB (46.79%)

--- my-folder/3684-substring-matching-pattern/solution.py
+++ my-folder/3684-substring-matching-pattern/solution.py
@@ -1,17 +1,11 @@
 class Solution:
     def hasMatch(self, s: str, p: str) -> bool:
         n = len(s)
-
-        prefix,_,suffix  = p.partition("*")
-        start_idx = s.find(prefix)
-        #print(f'prefix: {prefix}: start_idx: {start_idx}')
-        if start_idx==-1:return False
-
+        prefix, _, suffix = p.partition("*")
+        index = s.find(prefix)
+        if index==-1:
+            return False
         else:
-            extra = s[start_idx+len(prefix):]
+            extra = s[index+len(prefix):]
             return suffix in extra
             
-        
-
-        
-
