bc8a8d4b1a75a09cbcc90ff576d8b3730d85aacc
Ali John <63426759+ali-john@users.noreply.github.com>
2025-03-28 08:31:49 +0000

    [LeetCode Sync] Runtime - 7 ms (32.17%), Memory - 18.5 MB (24.77%)

--- my-folder/1253-sort-the-matrix-diagonally/solution.py
+++ my-folder/1253-sort-the-matrix-diagonally/solution.py
@@ -2,17 +2,21 @@ class Solution:
     def diagonalSort(self, mat: List[List[int]]) -> List[List[int]]:
         m = len(mat)
         n = len(mat[0])
+
         table = defaultdict(list)
         for i in range(m):
             for j in range(n):
                 table[i-j].append(mat[i][j])
-        for key,_ in table.items():
-            table[key].sort(reverse = True)
         
+        for key, _ in table.items():
+            table[key].sort()
+        
+        new_mat = [[-1]*n for _ in range(m)]
         for i in range(m):
             for j in range(n):
-                mat[i][j] = table[i-j].pop()
+                new_mat[i][j] = table[i-j].pop(0)
         
-        return mat
+        return new_mat
+        
         
 
