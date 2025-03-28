b1e759106f7a7e6134a81631310b6956d60e5c1a
Ali John <63426759+ali-john@users.noreply.github.com>
2025-03-28 10:50:27 +0000

    [LeetCode Sync] Runtime - 128 ms (85.80%), Memory - 28.6 MB (67.46%)

--- my-folder/1573-find-two-non-overlapping-sub-arrays-each-with-target-sum/solution.py
+++ my-folder/1573-find-two-non-overlapping-sub-arrays-each-with-target-sum/solution.py
@@ -1,28 +1,31 @@
 class Solution:
     def minSumOfLengths(self, nums: List[int], target: int) -> int:
+        # DP with sliding window
+
+
         n = len(nums)
-        windows = [float('inf')]*n
 
         left = 0
-        right = 0
-        total = 0
+        dp = [float('inf')]*n
+        running_sum = 0
+        min_len = float('inf')
         ans = float('inf')
-        minLen = float('inf')
-        while right<n:
-            total+=nums[right]
+        for right, num in enumerate(nums):
+            running_sum += nums[right]
 
-            while total>target:
-                total-=nums[left]
+            while running_sum > target:
+                running_sum -= nums[left]
                 left+=1
-            if total==target:
-                curr = right-left + 1
-                if left>0 and windows[left-1]<float('inf'):
-                    ans = min(ans,curr+windows[left-1])
-                minLen = min(minLen,curr)
+            
+            if running_sum == target: 
+                current_length = right - left + 1
+                if left > 0 and dp[left - 1] != float('inf'):
+                    ans = min(ans, dp[left - 1] + current_length)
                 
-            windows[right] = minLen
-            right+=1
-        return -1 if ans==float('inf') else ans
+                min_len = min(min_len, current_length)
+            dp[right] = min_len
+        
+        return ans if ans != float('inf') else -1
         
         
         
