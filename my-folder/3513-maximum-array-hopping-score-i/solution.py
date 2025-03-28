9bc38c437de05e32c9524a2f7985af7039ef61ec
Ali John <63426759+ali-john@users.noreply.github.com>
2025-03-28 09:54:54 +0000

    [LeetCode Sync] Runtime - 7108 ms (24.53%), Memory - 17.9 MB (64.15%)

--- my-folder/3513-maximum-array-hopping-score-i/solution.py
+++ my-folder/3513-maximum-array-hopping-score-i/solution.py
@@ -6,20 +6,18 @@ class Solution:
         # def dp(i):
         #     if i>=n: return 0
         #     ans = 0
-        #     for j in range(i+1,n):
-        #         take = dp(j) + nums[j] * (j-i)
-        #         ans = max(ans,take)
+        #     for j in range(i+1, n):
+        #         take = dp(j) + (j-i)*nums[j]
+        #         ans = max(ans, take)
         #     return ans
         # return dp(0)
 
         dp = [0]*n
-        
-        for i in range(n-1):
-            for j in range(i+1,n):
-                dp[j] = max(dp[j],dp[i] + nums[j]*(j-i))
+
+        for i in range(n):
+            for j in range(i+1, n):
+                dp[j] = max(dp[j],dp[i]+ (j-i)*nums[j] )
         return dp[-1]
-
-
 
 
 
