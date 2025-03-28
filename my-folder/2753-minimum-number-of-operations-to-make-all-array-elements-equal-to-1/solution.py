8f16049744e102f97b0df83ed806198e27c2c3c5
Ali John <63426759+ali-john@users.noreply.github.com>
2025-03-28 09:27:02 +0000

    [LeetCode Sync] Runtime - 4 ms (23.44%), Memory - 17.7 MB (57.81%)

--- my-folder/2753-minimum-number-of-operations-to-make-all-array-elements-equal-to-1/solution.py
+++ my-folder/2753-minimum-number-of-operations-to-make-all-array-elements-equal-to-1/solution.py
@@ -1,16 +1,26 @@
 class Solution:
     def minOperations(self, nums: List[int]) -> int:
         n = len(nums)
-        min_window = float('inf')
         ones = nums.count(1)
-        if ones: return n-ones
+
+        if ones:
+            return n - ones
+        
+        # check if you can make 1s
+        min_window = float('inf')
         for i in range(n):
             g = nums[i]
             for j in range(i+1,n):
                 g = gcd(g,nums[j])
-                if g ==1:
-                    min_window = min(min_window,(j-i))
+                if g == 1:
+                    min_window = min(min_window, (j-i))
         if min_window == float('inf'): return -1
-        op = min_window + n-1
-        return op
+        return min_window + n-1
+            
+# test case
+# [6, 10, 15] # we need to check if we can make the whole window gcd == 1
+# [6, 2, 15]
+# [6,2,1]
+# [6,1,1]
+# [1,1,1]
 
