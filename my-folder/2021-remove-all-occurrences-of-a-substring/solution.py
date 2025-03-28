8b3d575cbd0a0059d32b8132f3503b556da8f487
Ali John <63426759+ali-john@users.noreply.github.com>
2025-03-28 09:06:51 +0000

    [LeetCode Sync] Runtime - 19 ms (11.93%), Memory - 18.2 MB (12.78%)

--- my-folder/2021-remove-all-occurrences-of-a-substring/solution.py
+++ my-folder/2021-remove-all-occurrences-of-a-substring/solution.py
@@ -2,15 +2,15 @@ class Solution:
     def removeOccurrences(self, s: str, part: str) -> str:
         n = len(s)
         m = len(part)
-
         stack = []
 
         def is_match():
-            return "".join(stack[-m:])==part
-        
-        for i,char in enumerate(s):
+            return "".join(stack[-m:]) == part
+
+        for char in s:
             stack.append(char)
             if len(stack)>=m and is_match():
-                for _ in range(m):
+                for i in range(m):
                     stack.pop()
         return "".join(stack)
+
