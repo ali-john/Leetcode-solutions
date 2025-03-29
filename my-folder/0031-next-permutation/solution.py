8045ca82e1032d5cd3a1fece0eedb8d972f8c19a
Ali John <63426759+ali-john@users.noreply.github.com>
2025-03-29 10:13:59 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 18 MB (14.22%)

--- my-folder/0031-next-permutation/solution.py
+++ my-folder/0031-next-permutation/solution.py
@@ -6,17 +6,18 @@ class Solution:
         n = len(nums)
 
         i = n - 2
-        while i>=0 and nums[i]>=nums[i+1]:
-            i-=1
-        if i<0:
+
+        while i>=0 and nums[i] >= nums[i+1]:
+            i -=1
+        
+        if i < 0:
             nums.sort()
-            return 
+            return
         
         j = n - 1
-        while nums[j]<=nums[i]:
-            j-=1
+        while nums[j] <= nums[i]:
+            j -=1
         
         nums[j], nums[i] = nums[i], nums[j]
-        nums[i+1:] = reversed(nums[i+1:])
-        return nums
-        
+        nums[i+1: ] = reversed(nums[i+1: ])
+        return
