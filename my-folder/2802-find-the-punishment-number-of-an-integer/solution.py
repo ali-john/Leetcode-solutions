3704c8be85aa5e6768578593b048cc04b49b1aa3
Ali John <63426759+ali-john@users.noreply.github.com>
2025-04-15 17:04:58 +0000

    [LeetCode Sync] Runtime - 1072 ms (45.91%), Memory - 17.8 MB (76.34%)

--- my-folder/2802-find-the-punishment-number-of-an-integer/solution.py
+++ my-folder/2802-find-the-punishment-number-of-an-integer/solution.py
@@ -1,26 +1,23 @@
 class Solution:
     def punishmentNumber(self, n: int) -> int:
-        ans = 0
+        
+        def is_possible(target, num):
+            if not num and target == 0: return True
+            if target < 0: return False
 
-
-        def is_possible(target,str_num):
-            if not str_num and target ==0:
-                return True
-            if target<0: return False
-
-            for i in range(len(str_num)):
-                left = str_num[:i+1]
-                right = str_num[i+1:]
-                left_num = int(left)
-                if is_possible(target-left_num,right):
+            for i in range(len(num)):
+                left = num[:i+1]
+                right = num[i+1:]
+                if is_possible(target - int(left), right):
                     return True
             return False
 
 
-
-        for i in range(1,n+1):
+        ans = 0
+        for i in range(1, n+1):
             square = i**2
             if is_possible(i,str(square)):
-                ans+=(i**2)
+                ans += square
         return ans
+
 
