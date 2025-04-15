8da9ea2843a05fca9e1bb17a91dfeb55d1107cce
Ali John <63426759+ali-john@users.noreply.github.com>
2025-04-15 14:56:45 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 18 MB (11.32%)

---
+++ my-folder/3830-find-closest-person/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findClosest(self, x: int, y: int, z: int) -> int:
+        dist1 =  abs(x - z)
+        dist2 = abs(y - z)
+
+        if dist1 == dist2: return 0
+        elif dist1 < dist2: return 1
+        else: return 2
