22ee4352aa8d0793e0a8945a3f3e2094abb97095
Ali John <63426759+ali-john@users.noreply.github.com>
2025-04-15 16:13:28 +0000

    [LeetCode Sync] Runtime - 306 ms (41.56%), Memory - 20.3 MB (19.05%)

---
+++ my-folder/3812-smallest-palindromic-rearrangement-i/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def smallestPalindrome(self, s: str) -> str:
+        n = len(s)
+        forward = []
+        for i in range(n//2):
+            forward.append(s[i])
+
+        forward = "".join(sorted(forward))
+        backward = forward[::-1]
+        if n%2 !=0:
+            forward+=s[n//2]
+
+        return forward + backward
+                
+        
+    
