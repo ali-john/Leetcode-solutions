02f618c48606a3526492ef9641f07a40dc4936ce
Ali John <63426759+ali-john@users.noreply.github.com>
2025-04-15 18:09:18 +0000

    [LeetCode Sync] Runtime - 21 ms (62.22%), Memory - 19.8 MB (46.67%)

--- my-folder/2880-find-the-closest-marked-node/solution.py
+++ my-folder/2880-find-the-closest-marked-node/solution.py
@@ -7,18 +7,23 @@ class Solution:
         
         distances = [float('inf')]*n
         distances[s] = 0
-        heap = [(0,s)]
 
+        heap = [(0,s)]
         while heap:
-            weight,node = heapq.heappop(heap)
-            
-            for child,w in graph[node]:
-                if weight+w < distances[child]:
-                    distances[child] = weight+w
-                    heapq.heappush(heap,(weight+w,child))
+            dist , node = heapq.heappop(heap)
+
+            for nei, w in graph[node]:
+                new_w = w + dist
+                if new_w < distances[nei]:
+                    distances[nei] = new_w
+                    heapq.heappush(heap,(new_w,nei))
         
         min_distances = []
-        for m in marked:
-            dist = distances[m]
-            min_distances.append(dist)
-        return -1 if all(dist==float('inf') for dist in min_distances) else min(min_distances)
+        for node in marked:
+            min_distances.append(distances[node])
+        
+        return -1 if all(dist == float('inf') for dist in min_distances) else min(min_distances)
+        
+
+
+
