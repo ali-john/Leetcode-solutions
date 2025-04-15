b5c927d962fc299f8866e71a168b523578002d3f
Ali John <63426759+ali-john@users.noreply.github.com>
2025-04-15 17:12:54 +0000

    [LeetCode Sync] Runtime - 55 ms (32.24%), Memory - 22.5 MB (14.56%)

--- my-folder/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/solution.py
+++ my-folder/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/solution.py
@@ -1,21 +1,21 @@
 class Solution:
     def getHappyString(self, n: int, k: int) -> str:
         
-        total = []
-        def get(s):
-            if len(s) == n:
-                total.append(s)
-                return 
+        all_str = []
+
+        def generate(string):
+            if len(string) == n:
+                all_str.append(string)
+                return
             
             for char in ['a','b','c']:
-                if len(s)>0 and char==s[-1]: continue
-                get(s+char)
-        get("")
-        if len(total)<k: return ""
-        else:
-            total.sort()
-            return total[k-1]
-            
+                if len(string) > 0 and char == string[-1]: continue
+                generate(string+char)
 
+        generate("")
+        if k> len(all_str): return ""
+        else:
+            all_str.sort()
+            return all_str[k-1]
 
 
