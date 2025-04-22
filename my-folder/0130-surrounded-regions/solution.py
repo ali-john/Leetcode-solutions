47901a2b6f08346b9c2a82011122248c43e6aae2
Ali John <63426759+ali-john@users.noreply.github.com>
2025-04-22 19:00:50 +0000

    [LeetCode Sync] Runtime - 6 ms (61.89%), Memory - 21.5 MB (88.51%)

--- my-folder/0130-surrounded-regions/solution.py
+++ my-folder/0130-surrounded-regions/solution.py
@@ -3,29 +3,37 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
+        # logic:
+        # capture all region that are not surrounded. 
+        
+
         rows, cols = len(board), len(board[0])
 
         def dfs(r,c):
-            if (r<0 or r>=rows or c<0 or c>=cols or board[r][c]!='O'):
-                return
+            if (r < 0 or r >= rows or c < 0 or c >= cols or board[r][c]!='O'): return
             board[r][c] = 'T'
-            dfs(r+1,c)
-            dfs(r-1,c)
-            dfs(r,c+1)
-            dfs(r,c-1)
-        
+            dfs(r+1, c)
+            dfs(r-1, c)
+            dfs(r, c+1)
+            dfs(r, c - 1)
+
+
+
+
         for r in range(rows):
             for c in range(cols):
-                if ( board[r][c]== 'O' and  (r in [0,rows -1] or c in [0, cols-1]) ):
+                if board[r][c] == 'O' and (r in [0,rows -1 ] or c in [0, cols -1 ]):
                     dfs(r,c)
-        # replace all O with X
-        for i in range(rows):
-            for j in range(cols):
-                if board[i][j]=='O':
-                    board[i][j] = 'X'
+
+        for r in range(rows):
+            for c in range(cols):
+                if board[r][c] == 'O':
+                    board[r][c] = 'X'
         
         for i in range(rows):
             for j in range(cols):
                 if board[i][j] == 'T':
                     board[i][j] = 'O'
+        rows, cols = len(board), len(board[0])
+
         
