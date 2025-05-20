5cdf00a2e02efcace10d3f65140d3d5be9354a7c
Ali John <63426759+ali-john@users.noreply.github.com>
2025-05-20 00:14:47 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.7 MB (69.47%)

---
+++ my-folder/3143-longest-unequal-adjacent-groups-subsequence-i/solution.py
@@ -0,0 +1,6 @@
+class Solution:
+    def getLongestSubsequence(self, words: List[str], groups: List[int]) -> List[str]:
+        n = len(words)
+
+        return [words[0]] + [ words[i] for i in range(1,len(groups)) if groups[i]!=groups[i-1] ]
+
