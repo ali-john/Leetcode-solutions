ce978315ff37a06103ff9f3a5233173fcdbcc886
Ali John <63426759+ali-john@users.noreply.github.com>
2025-05-21 23:04:49 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 18.5 MB (53.97%)

--- my-folder/0073-set-matrix-zeroes/solution.py
+++ my-folder/0073-set-matrix-zeroes/solution.py
@@ -3,24 +3,23 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        to_set = []
-        col = []
-        for i in range(len(matrix)): # rows
-            for j in range(len(matrix[0])): # cols
-                if matrix[i][j]==0:
-                    to_set.append((i,j))
-        for each in to_set:
-            row,col = each
-            # set all row to 0:
+        m = len(matrix)
+        n = len(matrix[0])
+
+        indices = set()
+
+        for i in range(m):
+            for j in range(n):
+                if matrix[i][j] == 0:
+                    indices.add((i,j))
+        
+        for index in indices:
+            row,col = index
             for i in range(len(matrix[0])):
                 matrix[row][i] = 0
             # set all column to 0:
             for i in range(len(matrix)):
                 matrix[i][col] = 0
-        
 
             
-
-
-
         
