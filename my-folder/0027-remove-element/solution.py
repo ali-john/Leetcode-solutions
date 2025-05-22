393c97bfd6cf551c73ea79323d4c874ed48f5cd4
Ali John <63426759+ali-john@users.noreply.github.com>
2025-05-22 18:43:26 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.7 MB (53.02%)

--- my-folder/0027-remove-element/solution.py
+++ my-folder/0027-remove-element/solution.py
@@ -1,16 +1,15 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
+        n = len(nums)
 
+        ptr1 = 0
+        ptr2 = 0
 
-        nextPtr = 0
-        prevPtr = 0
-
-        while nextPtr < len(nums):
-            if nums[nextPtr]!=val:
-                nums[prevPtr] = nums[nextPtr]
-                prevPtr+=1
-            nextPtr+=1
-        return prevPtr
-
-
+        while ptr2 < n:
+            if nums[ptr2] != val:
+                nums[ptr1] = nums[ptr2]
+                ptr1+=1
+            
+            ptr2+=1
         
+        return ptr1
