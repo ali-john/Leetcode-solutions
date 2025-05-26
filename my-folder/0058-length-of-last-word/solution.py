212b136152282c67eb9b4b33a652f31d13f2a729
Ali John <63426759+ali-john@users.noreply.github.com>
2025-05-26 22:02:28 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.8 MB (61.01%)

--- my-folder/0058-length-of-last-word/solution.py
+++ my-folder/0058-length-of-last-word/solution.py
@@ -1,10 +1,22 @@
-class Solution(object):
-    def lengthOfLastWord(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        result = s.split()
-        return len(result[-1])
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        n = len(s)
 
+        end_index = -1
+        start_index = -1
+        for i in reversed(range(n)):
+            if s[i] != ' ' and end_index==-1:
+                end_index = i
+            
+            if s[i] == ' ' and end_index!=-1:
+                start_index = i
+                break
         
+        if start_index != -1:
+            return end_index - start_index
+        
+        else:
+            return end_index + 1
+
+            
+
