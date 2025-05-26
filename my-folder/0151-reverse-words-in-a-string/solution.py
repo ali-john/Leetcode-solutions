01617c9539218a7d30e86ff54d3dc86cf271807a
Ali John <63426759+ali-john@users.noreply.github.com>
2025-05-26 23:39:41 +0000

    [LeetCode Sync] Runtime - 3 ms (32.77%), Memory - 17.9 MB (43.65%)

--- my-folder/0151-reverse-words-in-a-string/solution.py
+++ my-folder/0151-reverse-words-in-a-string/solution.py
@@ -1,8 +1,31 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
-        res = s.split()
-        res = res[::-1]
-        output = ' '.join(res)
-        return output
-        
 
+        def split() -> list:
+            n = len(s)
+            out = []
+
+            i = 0
+            while i < n:
+                if s[i]!=' ':
+                    temp = s[i]
+                    for j in range(i+1, n):
+                        if s[j]!=' ':
+                            temp+=s[j]
+                            i = j
+                        else:
+                            i = j
+                            break
+                    out.append(temp)
+                i+=1
+            return out
+
+        
+        test = split()
+        ans = []
+        for i in reversed(range(len(test))):
+            if len(test[i]) > 0:
+                ans.append(test[i])
+                ans.append(' ')
+
+        return "".join(ans[:-1])
