7360de471c137a60deb3a495e9fee01f3d6f8ce3
Ali John <63426759+ali-john@users.noreply.github.com>
2025-06-04 20:03:29 +0000

    [LeetCode Sync] Runtime - 33 ms (17.45%), Memory - 17.9 MB (52.88%)

--- my-folder/0003-longest-substring-without-repeating-characters/solution.py
+++ my-folder/0003-longest-substring-without-repeating-characters/solution.py
@@ -1,19 +1,73 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        table = defaultdict(int)
         n = len(s)
-        left = 0
-        right = 0
+
+        table = defaultdict(int)
+        left = right = 0
 
         ans = 0
-        while right<n:
+        while right < n:
             table[s[right]]+=1
 
-            while left<=right and table[s[right]]>1:
+            while left <= right and table[s[right]] > 1:
                 table[s[left]]-=1
-                if table[s[left]]==0:
+                if table[s[left]] == 0:
                     del table[s[left]]
                 left+=1
-            ans = max(ans,(right-left+1))
+
+            ans = max(ans, (right - left) +1)
             right+=1
+        
         return ans
+
+
+
+
+
+
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
+
+
+
+
+
+
+
+
+
+        
+        
+        
+        
+        
+        # table = defaultdict(int)
+        # n = len(s)
+        # left = 0
+        # right = 0
+
+        # ans = 0
+        # while right<n:
+        #     table[s[right]]+=1
+
+        #     while left<=right and table[s[right]]>1:
+        #         table[s[left]]-=1
+        #         if table[s[left]]==0:
+        #             del table[s[left]]
+        #         left+=1
+        #     ans = max(ans,(right-left+1))
+        #     right+=1
+        # return ans
