1f7a8f88028226e40fd9ba8e35f87305d5130486
Ali John <63426759+ali-john@users.noreply.github.com>
2025-06-05 17:33:38 +0000

    [LeetCode Sync] Runtime - 15 ms (75.60%), Memory - 17.9 MB (54.26%)

--- my-folder/0383-ransom-note/solution.py
+++ my-folder/0383-ransom-note/solution.py
@@ -1,13 +1,13 @@
 class Solution:
-    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        output = True
-        for i in range(len(ransomNote)):
-            if ransomNote[i] in magazine:
-                magazine = magazine.replace(ransomNote[i],'',1)
-            else:
-                output=False
-                break
-        return output
+    def canConstruct(self, r: str, m: str) -> bool:
+        n = len(r)
+        r_table = Counter(r)
+        m_table = Counter(m)
 
+        for char in r:
+            if char in m_table and m_table[char] >= r_table[char]:
+                continue
             
-        
+            else:
+                return False
+        return True
