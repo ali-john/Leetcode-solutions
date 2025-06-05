0c4b61eb0d6144bd3e2a5a8cfdc145342d6a6ea9
Ali John <63426759+ali-john@users.noreply.github.com>
2025-06-05 19:39:16 +0000

    [LeetCode Sync] Runtime - 29 ms (64.84%), Memory - 36.7 MB (38.78%)

--- my-folder/0219-contains-duplicate-ii/solution.py
+++ my-folder/0219-contains-duplicate-ii/solution.py
@@ -1,18 +1,12 @@
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-        output = False
-        for i in range(len(nums)-1):
-            if i+k<=len(nums):
-                sub_arr = nums[i+1:(i+k)+1]
-                #print(sub_arr)
-                if nums[i] in sub_arr:
-                    output=True
-                    break
-            else:
-                sub_arr = nums[i+1:len(nums)]
-                if nums[i] in sub_arr:
-                    output = True
-                    break
-        return output
+        table = defaultdict()
+        n = len(nums)
 
-        
+        for i, num in enumerate(nums):
+            if num in table:
+                dist = i - table[num]
+                if dist <= k:
+                    return True
+            table[num] = i
+        return False
