f459f3a84245faaa857c6e4ed33af52c5be472f7
Ali John <63426759+ali-john@users.noreply.github.com>
2025-06-06 00:11:09 +0000

    [LeetCode Sync] Runtime - 138 ms (7.24%), Memory - 45.3 MB (5.05%)

--- my-folder/0128-longest-consecutive-sequence/solution.py
+++ my-folder/0128-longest-consecutive-sequence/solution.py
@@ -1,15 +1,43 @@
+
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
+
         nums = set(nums)
-        table = {}
-        maxval = 0
+        n = len(nums)
+
+        table = defaultdict()
+        ans = 0
         for num in nums:
-            x = table.get(num - 1, 0)
-            y = table.get(num + 1, 0)
-            val = x+y+1
-            table[num-x] = val
-            table[num+y] = val
-            maxval = max(val,maxval)
-        return maxval
+            x = table.get(num - 1, 0 )
+            y = table.get(num +1, 0)
+            val = x + y + 1
+            table[num - x] = val
+            table[num + y] = val
+            ans = max(val,ans)
+        return ans
+
+
+       
+
+
         
+
+        # sol 2
+
+        # nums = set(nums)
+
+        # if len(nums) == 0:
+        #     return 0
+        # ans = float("-inf")
+
+        # for num in nums:
+        #     length = 1
+        #     if num - 1 not in nums:
+        #         while num + length in nums:
+        #             length+=1
+        #     ans = max(length,ans)
+        # return ans
+        
+
+
         
