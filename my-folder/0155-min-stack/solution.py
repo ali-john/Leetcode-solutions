0b8a9c0ac97ede238d55ebab7302c78b0b4d91a1
Ali John <63426759+ali-john@users.noreply.github.com>
2025-06-06 23:13:06 +0000

    [LeetCode Sync] Runtime - 3 ms (83.21%), Memory - 21.5 MB (34.84%)

--- my-folder/0155-min-stack/solution.py
+++ my-folder/0155-min-stack/solution.py
@@ -1,30 +1,25 @@
 class MinStack:
-    # Two stack approach. Maintain a min stack to keep track of min value       #encountered so for
+
     def __init__(self):
-        self.stack = deque()
-        self.min_stack = deque()
-        
+        self.stack = []
+        self.minStack = []
+    
     def push(self, val: int) -> None:
-        if len(self.stack) == 0 or self.min_stack[-1] >= val:
-            self.min_stack.append(val)
-            self.stack.append(val)
-        else:
-            self.stack.append(val)
+        self.stack.append(val)
+        val = min(val,self.minStack[-1] if self.minStack else val) # set minimum value at each step in minStack
+        self.minStack.append(val)
         
     def pop(self) -> None:
-        if self.min_stack[-1] == self.stack[-1]:
-            self.min_stack.pop()
-            self.stack.pop()
-        else:
-            self.stack.pop()
+        self.stack.pop()
+        self.minStack.pop()
         
     def top(self) -> int:
-        t = self.stack[-1]
-        return t
+        return self.stack[-1]
         
     def getMin(self) -> int:
-        m = self.min_stack[-1]
-        return m
+        return self.minStack[-1]
+        
+
 
 # Your MinStack object will be instantiated and called as such:
 # obj = MinStack()
