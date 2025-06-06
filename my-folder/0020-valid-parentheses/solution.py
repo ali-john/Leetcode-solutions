14d6661bc443783b0154ff662f0fa2734de1d633
Ali John <63426759+ali-john@users.noreply.github.com>
2025-06-06 21:08:23 +0000

    [LeetCode Sync] Runtime - 4 ms (12.74%), Memory - 17.9 MB (58.11%)

--- my-folder/0020-valid-parentheses/solution.py
+++ my-folder/0020-valid-parentheses/solution.py
@@ -1,42 +1,21 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        if len(s) == 1:
-            return False
-        stack = collections.deque()
-        open_p = ['(','{','[']
-        closed_p = [')','}',']']
-        output = True
-        close_p = False
-        for i in range(len(s)):
-            val = s[i]
-            if val in open_p:
-                stack.append(val)
+        n = len(s)
+        
+
+        stack = []
+
+        for char in s:
+            if char in ['(', '{', '[']:
+                stack.append(char)
             else:
-                close_p = True
-                if len(stack)!=0:
-                    top = stack.pop()
-                    if top == '(':
-                        if val != ')':
-                            return False
-                    elif top=='{':
-                        if val != '}':
-                            return False
-                    elif top=='[':
-                        if val != ']':
-                            return False
+                if len(stack) > 0:
+                    if char == ')' and stack[-1]!= '(':
+                        return False
+                    elif char == '}' and stack[-1]!='{': return False
+                    elif len(stack)>0 and char == ']' and stack[-1]!='[': return False
+                    else:
+                        stack.pop(-1)
                 else:
                     return False
-
-        if close_p:     
-            if len(stack)==0:
-                return True
-            else:
-                return False
-        else:
-            return False
-
-                
-
-
-        
-        
+        return len(stack)==0
