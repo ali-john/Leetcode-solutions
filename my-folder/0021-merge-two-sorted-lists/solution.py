3d6b585a636beb4f9bf6f30cc09b4c1cf043cc7c
Ali John <63426759+ali-john@users.noreply.github.com>
2025-06-07 18:54:26 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.9 MB (17.92%)

--- my-folder/0021-merge-two-sorted-lists/solution.py
+++ my-folder/0021-merge-two-sorted-lists/solution.py
@@ -4,63 +4,36 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        output = ListNode()
-        head = output #store starting point of resultant list
-        temp = output
-        l1 = list1
-        l2 = list2
-        first = l1.val if l1 is not None else None
-        second = l2.val if l2 is not None else None
+    def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummyHead = ListNode(0,None)
+        temp = dummyHead
 
-        while l1 is not None or l2 is not None:
-            if first is not None and second is not None:
-                if first<second:
-                    NewNode = ListNode(first)
-                    temp.next = NewNode
-                    temp = NewNode
-                    l1 = l1.next if l1 is not None else None
-                    first = l1.val if l1 is not None else None
-                elif second<first:
-                    NewNode = ListNode(second)
-                    temp.next = NewNode
-                    temp = NewNode
-                    l2 = l2.next if l2 is not None else None
-                    second = l2.val if l2 is not None else None
-                else:
-                    NewNode1 = ListNode(first)
-                    NewNode2 = ListNode(second)
-                    NewNode1.next = NewNode2
-                    temp.next = NewNode1
-                    temp = NewNode2
-                    l1 = l1.next if l1 is not None else None
-                    first = l1.val if l1 is not None else None
-                    l2 = l2.next if l2 is not None else None
-                    second = l2.val if l2 is not None else None
+        while l1 is not None and l2 is not None:
+            newNode = ListNode(0,None)
 
-            else: # One list is completed. Add the other now
-                if l1 is None:
-                    while l2 is not None:
-                        NewNode = ListNode(second)
-                        temp.next = NewNode
-                        temp = NewNode
-                        l2 = l2.next if l2 is not None else None
-                        second = l2.val if l2 is not None else None
-                else:
-                    while l1 is not None:
-                        NewNode = ListNode(first)
-                        temp.next = NewNode
-                        temp = NewNode
-                        l1 = l1.next if l1 is not None else None
-                        first = l1.val if l1 is not None else None
-                    
+            if l1.val <= l2.val:
+                newNode.val = l1.val
+                l1 = l1.next if l1 is not None else None
+            elif l2 is not None:
+                newNode.val = l2.val
+                l2 = l2.next if l2 is not None else None
+            
+            temp.next = newNode
+            temp = newNode
         
-        result = head.next
-        return result
-
-
-
-
+        while l1 is not None:
+            newNode = ListNode(l1.val,None)
+            temp.next = newNode
+            temp = newNode
+            l1 = l1.next if l1 is not None else None
+        
+        while l2 is not None:
+            newNode = ListNode(l2.val,None)
+            temp.next = newNode
+            temp = newNode
+            l2 = l2.next if l2 is not None else None
+        
 
 
         
+        return dummyHead.next
