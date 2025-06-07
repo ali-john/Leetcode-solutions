5e48908bd9ce0cb1dcc08b663c9c078140ac842d
Ali John <63426759+ali-john@users.noreply.github.com>
2025-06-07 19:34:12 +0000

    [LeetCode Sync] Runtime - 36 ms (86.18%), Memory - 18.5 MB (59.83%)

--- my-folder/0138-copy-list-with-random-pointer/solution.py
+++ my-folder/0138-copy-list-with-random-pointer/solution.py
@@ -9,70 +9,45 @@ class Node:
 
 class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
-        # make a new list
-        temp = head
-        head2 = Node(0)
-        temp2 = head2
-        random_pos = {}
-        while temp is not None: # Create new nodes 
-            val = temp.val
-            NewNode = Node(val)
-            temp2.next = NewNode
-            temp2 = NewNode
-            temp = temp.next if temp is not None else None
+        dummy = Node(0)
+        temp = dummy
 
-        temp = head
-        present_node=0
-        while temp is not None: # Create hashtable for random pointers
-            next_node = temp.random
-            node_number = 0
-            if next_node is None:
-                random_pos[present_node] = None
-                present_node+=1
-                temp = temp.next if temp is not None else None
-            else:
-                next_data = next_node.val
-                temp3 = head
-                while not(temp3.val == next_data and temp3==next_node):
-                    node_number+=1
-                    temp3 = temp3.next
-                random_pos[present_node] = node_number
-                present_node+=1
-                temp = temp.next if temp is not None else None
+        start = head
 
-        head2 = head2.next
-        temp = head2
         i = 0
-        while temp is not None:
-            pos = random_pos[i]
-            if pos is None:
+        node_to_index_old = defaultdict()
+        index_to_node_old = defaultdict()
+        node_to_index_new = defaultdict()
+        index_to_node_new = defaultdict()
+
+        while start is not None:
+            newNode = Node(start.val)
+            temp.next = newNode
+            temp = newNode
+            
+            node_to_index_old[start] = i
+            node_to_index_new[newNode] = i
+
+            index_to_node_old[i] = start
+            index_to_node_new[i] = newNode
+
+            start = start.next if start else None
+            i+=1
+        
+        temp = dummy.next
+        start = head
+
+        while start is not None:
+            if start.random is None:
                 temp.random = None
             else:
-                temp1 = head2
-                j = 0
-                while j!= pos:
-                    temp1 = temp1.next
-                    j+=1
-                temp.random = temp1
-            i+=1
-            temp = temp.next if temp is not None else None
-
-        return head2
-
-
-
-
-                
-
-                
-
-
+                pointsTo = index_to_node_new[ node_to_index_old[start.random] ]
+                temp.random = pointsTo
             
-
-
+            start = start.next if start else None
+            temp = temp.next if temp else None
+        
+        return dummy.next
+                
         
 
-
-
-        
-        
