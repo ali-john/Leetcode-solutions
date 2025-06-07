c511e208d2c853dc6b09d59880fabb4e3b78b10d
Ali John <63426759+ali-john@users.noreply.github.com>
2025-06-07 21:45:29 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.9 MB (56.36%)

--- my-folder/0092-reverse-linked-list-ii/solution.py
+++ my-folder/0092-reverse-linked-list-ii/solution.py
@@ -1,26 +1,60 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
     def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
-        if not head or left == right:
+        if head.next is None:
             return head
         
-        dummy = ListNode(0)
-        dummy.next = head
-        prev = dummy
         
-        # Step 1: Reach the node just before the "left" position
-        for _ in range(left - 1):
-            prev = prev.next
+        stack = []
+        temp = head
+        i = 1
+
+        while temp is not None:
+            if left <= i <= right:
+                stack.append(temp.val)
+            i+=1
+            temp = temp.next
         
-        # `start` will eventually be the tail of the reversed sublist
-        start = prev.next
-        then = start.next
+        temp = head
+        i = 1
+        while temp is not None:
+            if left <= i <= right:
+                temp.val = stack.pop()
+            temp = temp.next
+            i+=1
+        temp = head
+
+        # while temp:
+        #     print(temp.val)
+        #     temp = temp.next
+        return head
+
+
         
-        # Step 2: Reverse the sublist from "left" to "right"
-        for _ in range(right - left):
-            start.next = then.next
-            then.next = prev.next
-            prev.next = then
-            then = start.next
+        # i = 1
+        # left_node = None
+        # right_node = None
+        # starting_node = None
+
+
+        # temp = head
+        # while temp:
+        #     if i == left:
+        #         starting_node = temp
+        #     elif i == right:
+        #         right_node = temp.next
+            
+        #     if starting_node is None:
+        #         left_node = temp
+        #     i+=1
+        #     temp = temp.next if temp else None
         
-        return dummy.next
+        # slow = starting_node
+        # fast = starting_node.next
+        # while slow:
+
 
