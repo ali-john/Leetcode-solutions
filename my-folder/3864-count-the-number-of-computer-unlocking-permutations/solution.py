68b9d350e237b71b0e3ee5ac351df07d0b55624d
Ali John <63426759+ali-john@users.noreply.github.com>
2025-06-09 17:05:18 +0000

    [LeetCode Sync] Runtime - 7 ms (100.00%), Memory - 32.1 MB (29.79%)

---
+++ my-folder/3864-count-the-number-of-computer-unlocking-permutations/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def countPermutations(self, complexity: List[int]) -> int:
+        MOD = 1000000007
+        n = len(complexity)
+
+        for i in range(1, n):
+            if complexity[i] <= complexity[0]:
+                return 0
+        
+        fact = 1
+        for i in range(1,n):
+            fact = (fact * i)%MOD
+        return fact%MOD
