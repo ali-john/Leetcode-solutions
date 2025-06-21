6e3c0e1e2daa3b8119005d446b54dbe0f153ebb5
Ali John <63426759+ali-john@users.noreply.github.com>
2025-06-21 05:01:12 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.9 MB (39.82%)

--- my-folder/0100-same-tree/solution.py
+++ my-folder/0100-same-tree/solution.py
@@ -5,26 +5,54 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def inOrder(self,tree,output=[]):
-        if tree is None:
-            output.append(None)
-            return
-        else:
-            output.append(tree.val)
-            self.inOrder(tree.left,output)
-            self.inOrder(tree.right,output)
-
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        pout = []
-        qout = []
-        self.inOrder(p,output=pout)
-        self.inOrder(q,output = qout)
+        
+        def is_same(p,q):
+            if not p and not q:
+                return True
+            if not p or not q:
+                return False
+            if p.val!=q.val:
+                return False
+            
+            return is_same(p.right,q.right) and is_same(p.left,q.left)
+        
+        return is_same(p,q)
 
-        if pout == qout:
-            return True
-        else:
-            return False
 
+        # BFS Approach
+        # if not p and not q:
+        #     return True
+        # if not p or not q:
+        #     return False
+
+        # def bfs(node) -> list:
+        #     que = [node]
+        #     ans = []
+        #     while que:
+        #         top = que.pop(0)
+        #         ans.append(top.val)
+
+        #         if top.left is not None:
+        #             que.append(top.left)
+        #             ans.append(top.left.val)
+        #         else:
+        #             ans.append("#")
+                
+        #         if top.right is not None:
+        #             que.append(top.right)
+        #             ans.append(top.right.val)
+        #         else:
+        #             ans.append("#")
+            
+        #     return ans
+        
+        # p_l = bfs(p)
+        # q_l = bfs(q)
+        # print(p_l)
+        # print(q_l)
+        # return p_l == q_l
+            
 
 
         
