3a4c272e293595148e7393ecb6d02ae72f544cf4
Ali John <63426759+ali-john@users.noreply.github.com>
2025-09-02 23:45:13 +0000

    [LeetCode Sync] Runtime - 110 ms (5.05%), Memory - 26.8 MB (67.43%)

--- my-folder/3566-find-the-sequence-of-strings-appeared-on-the-screen/solution.py
+++ my-folder/3566-find-the-sequence-of-strings-appeared-on-the-screen/solution.py
@@ -1,28 +1,19 @@
 class Solution:
     def stringSequence(self, target: str) -> List[str]:
         n = len(target)
-        output = []
-        string = ["a"]
-
-        ptr = 0
-        for i in range(n):
-            char = target[i]
-            if char == string[ptr]:
-                output.append("".join(string))
+        ans = ["a"]
+        i = 0
+        while i < n:
+            last = ans[-1]
+            if last == target: break
+            if last[-1] == target[i]:
+                ans.append(last + "a")
+                i += 1
             else:
-                output.append("".join(string))
-                while char!=string[ptr]:
-                    next_char = chr(((ord(string[ptr]) - ord('a') + 1) % 26) + ord('a'))
-                    string[ptr] = next_char
-                    output.append("".join(string))
-            string.append('a')
-            ptr+=1
-        return output
+                last_chars = list(last)
+                last_chars[-1] = chr(ord(last_chars[-1]) + 1)
+                ans.append("".join(last_chars))
+        return ans
 
-            
-
-
-            
-            
 
 
