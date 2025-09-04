ab32e21e99fef7743502aff5687aa9cdece66894
Ali John <63426759+ali-john@users.noreply.github.com>
2025-09-04 18:18:13 +0000

    [LeetCode Sync] Runtime - 753 ms (30.82%), Memory - 19.8 MB (39.27%)

--- my-folder/3860-resulting-string-after-adjacent-removals/solution.py
+++ my-folder/3860-resulting-string-after-adjacent-removals/solution.py
@@ -2,25 +2,18 @@ class Solution:
     def resultingString(self, s: str) -> str:
         n = len(s)
 
-        stack = []
-
         def is_consecutive(a,b):
-            if ( a == 'a' and b == 'z') or (a == 'z' and b == 'a'): return True
-            if abs( ord(a) - ord(b) ) == 1:
-                return True
-            else:
-                return False
- 
-        for i,char in enumerate(s):
-            if stack:
-                if is_consecutive(stack[-1],char):
-                    stack.pop()
-
-                else:
-                    stack.append(char)
-            else:
-                stack.append(char)
-
-        return "".join(stack)
+            if (a== 'a' and b== 'z') or (a=='z' and b == 'a'): return True
+            if abs( ord(a) - ord(b) ) == 1: return True
+            else: return False
         
-                
+        stack = []
+        for i in range(n):
+            current = s[i]
+            if stack and is_consecutive(current, stack[-1]):
+                stack.pop()
+            else:
+                stack.append(current)
+        return "".join(stack)
+
+
