351e7151d56d304167e122cdb215669a13e439ed
Ali John <63426759+ali-john@users.noreply.github.com>
2025-09-04 18:46:04 +0000

    [LeetCode Sync] Runtime - 133 ms (79.03%), Memory - 42.9 MB (78.28%)

--- my-folder/3894-maximize-ysum-by-picking-a-triplet-of-distinct-xvalues/solution.py
+++ my-folder/3894-maximize-ysum-by-picking-a-triplet-of-distinct-xvalues/solution.py
@@ -1,25 +1,55 @@
 class Solution:
     def maxSumDistinctTriplet(self, x: List[int], y: List[int]) -> int:
-
-
         n = len(x)
         table = defaultdict(int)
 
-        for i, num in enumerate(x):
-            if num in table:
-                table[num] = max(table[num], y[i])
+        for i in range(n):
+            x_val = x[i]
+            if x_val in table:
+                table[x_val] = max(table[x_val], y[i])
             else:
-                table[num] = y[i]
-
-        if len(table.keys()) < 3:
-            return -1
+                table[x_val] = y[i]
         
+        if len(table) <3: return -1
         vals = sorted(table.values(), reverse = True)
-        ans = 0
-        for i in range(3):
-            ans+=vals[i]
+        return sum(vals[:3])
 
-        return ans
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # n = len(x)
+        # table = defaultdict(int)
+
+        # for i, num in enumerate(x):
+        #     if num in table:
+        #         table[num] = max(table[num], y[i])
+        #     else:
+        #         table[num] = y[i]
+
+        # if len(table.keys()) < 3:
+        #     return -1
+        
+        # vals = sorted(table.values(), reverse = True)
+        # ans = 0
+        # for i in range(3):
+        #     ans+=vals[i]
+
+        # return ans
         
         
         
