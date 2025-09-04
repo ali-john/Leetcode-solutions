ad974a373ede649233ac300adf1a80ea0aadb446
Ali John <63426759+ali-john@users.noreply.github.com>
2025-09-04 18:33:01 +0000

    [LeetCode Sync] Runtime - 284 ms (51.08%), Memory - 20.6 MB (10.82%)

--- my-folder/3812-smallest-palindromic-rearrangement-i/solution.py
+++ my-folder/3812-smallest-palindromic-rearrangement-i/solution.py
@@ -4,13 +4,39 @@ class Solution:
         forward = []
         for i in range(n//2):
             forward.append(s[i])
-
+        
         forward = "".join(sorted(forward))
         backward = forward[::-1]
-        if n%2 !=0:
+        if n%2 != 0: # odd string
             forward+=s[n//2]
-
         return forward + backward
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # n = len(s)
+        # forward = []
+        # for i in range(n//2):
+        #     forward.append(s[i])
+
+        # forward = "".join(sorted(forward))
+        # backward = forward[::-1]
+        # if n%2 !=0:
+        #     forward+=s[n//2]
+
+        # return forward + backward
                 
         
     
