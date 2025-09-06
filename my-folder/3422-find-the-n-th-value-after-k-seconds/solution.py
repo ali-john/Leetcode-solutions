ec0b8ba6f92374864e37055302568dedbefdcbb5
Ali John <63426759+ali-john@users.noreply.github.com>
2025-09-06 22:12:00 +0000

    [LeetCode Sync] Runtime - 4308 ms (44.68%), Memory - 18 MB (48.94%)

--- my-folder/3422-find-the-n-th-value-after-k-seconds/solution.py
+++ my-folder/3422-find-the-n-th-value-after-k-seconds/solution.py
@@ -1,10 +1,10 @@
 class Solution:
     def valueAfterKSeconds(self, n: int, k: int) -> int:
         MOD = 1000000007
-
         arr = [1]*n
+
         while k:
-            for i in range(1,n):
-                arr[i] = (arr[i-1]+arr[i])%MOD
+            for i in range(1, n):
+                arr[i] = (arr[i] + arr[i-1])%MOD
             k-=1
         return arr[n-1]%MOD
