a83d7f49e7afa25fcd39a3495763538ea51abe2c
Ali John <63426759+ali-john@users.noreply.github.com>
2025-09-06 01:22:06 +0000

    [LeetCode Sync] Runtime - 199 ms (31.53%), Memory - 58.3 MB (29.73%)

--- my-folder/3526-minimum-number-of-flips-to-make-binary-grid-palindromic-i/solution.py
+++ my-folder/3526-minimum-number-of-flips-to-make-binary-grid-palindromic-i/solution.py
@@ -1,34 +1,33 @@
 class Solution:
     def minFlips(self, grid: List[List[int]]) -> int:
-        m  = len(grid) # rows
-        n = len(grid[0]) # cols
-        
-        row_operation = 0
+        m = len(grid)
+        n = len(grid[0])
+
+        row_ops = 0
         for i in range(m):
             row = grid[i]
-            startPtr = 0
-            endPtr = len(row)-1
-            while startPtr<=endPtr:
-                if row[startPtr]!=row[endPtr]:
-                    row_operation+=1
-                startPtr+=1
-                endPtr-=1
+            start = 0
+            end = n - 1
+            while start <= end:
+                if row[start]!=row[end]:
+                    row_ops+=1
+                start+=1
+                end-=1
         
-        col_operation = 0
-        
+        col_ops = 0
+
         for i in range(n):
             col = []
             for j in range(m):
                 col.append(grid[j][i])
             
-            startPtr = 0
-            endPtr = len(col)-1
-            while startPtr<=endPtr:
-                if col[startPtr]!=col[endPtr]:
-                    col_operation+=1
-                startPtr+=1
-                endPtr-=1
-            
-        return min(row_operation,col_operation)
+            start = 0
+            end = len(col) - 1
+            while start <= end:
+                if col[start]!=col[end]:
+                    col_ops+=1
+                start+=1
+                end-=1
+        return min(row_ops, col_ops)
 
-
+                
