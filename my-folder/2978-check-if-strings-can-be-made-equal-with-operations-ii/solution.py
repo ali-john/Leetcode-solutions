9ba3e2a008d95c2eadae3d865d98f3c1e3d0964d
Ali John <63426759+ali-john@users.noreply.github.com>
2025-09-11 22:10:35 +0000

    [LeetCode Sync] Runtime - 87 ms (88.07%), Memory - 19.2 MB (31.19%)

--- my-folder/2978-check-if-strings-can-be-made-equal-with-operations-ii/solution.py
+++ my-folder/2978-check-if-strings-can-be-made-equal-with-operations-ii/solution.py
@@ -1,33 +1,26 @@
 class Solution:
     def checkStrings(self, s1: str, s2: str) -> bool:
-        c1 = Counter(s1)
-        c2 = Counter(s2)
-
-        if c1!=c2:
-            return False
         
-        grp1_e = []
-        grp1_o = []
-        grp2_e = []
-        grp2_o = []
-
-        for i,char in enumerate(s1):
-            if i%2==0:
-                grp1_e.append(char)
-            else:
-                grp1_o.append(char)
-        for i,char in enumerate(s2):
-            if i%2==0:
-                grp2_e.append(char)
-            else:
-                grp2_o.append(char)
+        return (
+            ( sorted(s1[::2]) == sorted(s2[::2]) )
+            and 
+            ( sorted(s1[1::2]) == sorted(s2[1::2]) )
+        )
         
-        grp1_e = sorted(grp1_e)
-        grp1_o = sorted(grp1_o)
-        grp2_e = sorted(grp2_e)
-        grp2_o = sorted(grp2_o)
+        # n = len(s1)
+        # even_list_s1 = []
+        # even_list_s2 = []
 
-        return grp1_e==grp2_e and grp1_o==grp2_o
+        # odd_list_s1 = []
+        # odd_list_s2 = []
+        # for i in range(n):
+        #     if i%2==0:
+        #         even_list_s1.append(s1[i])
+        #         even_list_s2.append(s2[i])
+        #     else:
+        #         odd_list_s1.append(s1[i])
+        #         odd_list_s2.append(s2[i])
         
-
+        # return ( sorted(even_list_s1) == sorted(even_list_s2) ) and (
+        #     sorted(odd_list_s1) == sorted(odd_list_s2) )
 
