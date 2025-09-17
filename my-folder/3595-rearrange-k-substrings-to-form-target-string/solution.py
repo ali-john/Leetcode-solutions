efe4b933d5d4ad04efe02c304818cc947784da58
Ali John <63426759+ali-john@users.noreply.github.com>
2025-09-17 19:24:31 +0000

    [LeetCode Sync] Runtime - 181 ms (70.53%), Memory - 25.1 MB (51.58%)

--- my-folder/3595-rearrange-k-substrings-to-form-target-string/solution.py
+++ my-folder/3595-rearrange-k-substrings-to-form-target-string/solution.py
@@ -1,18 +1,47 @@
 class Solution:
     def isPossibleToRearrange(self, s: str, t: str, k: int) -> bool:
         n = len(s)
-        str1 = []
-        str2 = []
+        t_table = defaultdict(int)
+        s_table = defaultdict(int)
         k = n//k
+        for i in range(0,n,k):
+            t_table[t[i:i+k]] += 1
         
         for i in range(0,n,k):
-            str1.append(s[i:i+k])
-            str2.append(t[i:i+k])
+            sub = s[i:i+k]
+            s_table[sub]+=1
+        
+        return t_table == s_table
 
+
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # n = len(s)
+        # str1 = []
+        # str2 = []
+        # k = n//k
+        # for i in range(0,n,k):
+        #     str1.append(s[i:i+k])
+        #     str2.append(t[i:i+k])
        
-        str1 = sorted(str1)
-        str2 = sorted(str2)
-        str1 = ''.join(str1)
-        str2 = ''.join(str2)
-        return str1==str2
+        # str1 = sorted(str1)
+        # str2 = sorted(str2)
+        # str1 = ''.join(str1)
+        # str2 = ''.join(str2)
+        # return str1==str2
             
