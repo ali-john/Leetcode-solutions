e1189583a88a5ca09b5e341f94ec09a7d2278c92
Ali John <63426759+ali-john@users.noreply.github.com>
2025-09-29 22:37:56 +0000

    [LeetCode Sync] Runtime - 78 ms (19.66%), Memory - 50.9 MB (94.30%)

--- my-folder/3541-report-spam-message/solution.py
+++ my-folder/3541-report-spam-message/solution.py
@@ -1,22 +1,9 @@
 class Solution:
     def reportSpam(self, message: List[str], bannedWords: List[str]) -> bool:
-        table = defaultdict()
-        for char in message:
-            if char in table:
-                table[char]+=1
-            else:
-                table[char]=1
+        table = Counter(bannedWords)
+        ans = 0
+        for word in message:
+            if word in table:
+                ans+=1
         
-        count=0
-        for char in bannedWords:
-            if char in table:
-                if table[char]>=2:
-                    return True
-                count+=1
-                if count>=2:
-                    return True
-                table[char]-=1
-                if table[char]<=0:
-                    del table[char]
-        return False
-
+        return True if ans >=2 else False
