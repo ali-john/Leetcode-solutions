75a6c84299aa06acbab7d5c5884d1e0a8b47a933
Ali John <63426759+ali-john@users.noreply.github.com>
2025-09-29 22:32:41 +0000

    [LeetCode Sync] Runtime - 16 ms (19.54%), Memory - 17.7 MB (77.25%)

--- my-folder/3748-sort-matrix-by-diagonals/solution.py
+++ my-folder/3748-sort-matrix-by-diagonals/solution.py
@@ -1,21 +1,58 @@
 class Solution:
     def sortMatrix(self, mat: List[List[int]]) -> List[List[int]]:
         n = len(mat)
-        table = defaultdict(list)
 
+        table = defaultdict(list)
         for i in range(n):
             for j in range(n):
                 table[i-j].append(mat[i][j])
-        
-        for key,_ in table.items():
-            if key<0:
-                table[key].sort()
+        ans = [[0]*n for _ in range(n)]
+        for key,val in table.items():
+            if key < 0:
+                val.sort()
             else:
-                table[key].sort(reverse = True)
-        new_mat = [ [0]*n for _ in range(n)]
-        #print(table)
+                val.sort(reverse = True)
+        
         for i in range(n):
             for j in range(n):
-                new_mat[i][j] = table[i-j].pop(0)
-        return new_mat
+                ans[i][j] = (table[i-j].pop(0))
+        return ans
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # n = len(mat)
+        # table = defaultdict(list)
+
+        # for i in range(n):
+        #     for j in range(n):
+        #         table[i-j].append(mat[i][j])
+        
+        # for key,_ in table.items():
+        #     if key<0:
+        #         table[key].sort()
+        #     else:
+        #         table[key].sort(reverse = True)
+        # new_mat = [ [0]*n for _ in range(n)]
+        # #print(table)
+        # for i in range(n):
+        #     for j in range(n):
+        #         new_mat[i][j] = table[i-j].pop(0)
+        # return new_mat
 
