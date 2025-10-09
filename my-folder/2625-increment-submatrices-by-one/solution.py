247a227de5443f935495518b9094156a301a9e01
Ali John <63426759+ali-john@users.noreply.github.com>
2025-10-09 01:09:39 +0000

    [LeetCode Sync] Runtime - 843 ms (22.11%), Memory - 38.2 MB (58.42%)

--- my-folder/2625-increment-submatrices-by-one/solution.py
+++ my-folder/2625-increment-submatrices-by-one/solution.py
@@ -1,16 +1,16 @@
 class Solution:
     def rangeAddQueries(self, n: int, queries: List[List[int]]) -> List[List[int]]:
-        matrix = [[0]*n for i in range(n)]
-        # add 1 to every row's row1,col1 position and -1 to every row's r,col2+1 position
+        matrix = [[0]*n for _ in range(n)]
+
         for row1,col1,row2,col2 in queries:
             for r in range(row1,row2+1):
                 matrix[r][col1]+=1
-                if col2+1 <n:
-                    matrix[r][col2+1]-=1
-        # calculate prefix sum now
+                if col2+1 < n:
+                    matrix[r][col2+1] -=1
+        
+        
         for i in range(n):
             row = matrix[i]
             for j in range(1,n):
-                row[j] = row[j]+row[j-1]
-
+                row[j] = row[j] + row[j-1]
         return matrix
