dc3ba7df16016bc5292c665d62dd0c181c161fb1
Ali John <63426759+ali-john@users.noreply.github.com>
2025-10-09 01:25:26 +0000

    [LeetCode Sync] Runtime - 132 ms (59.30%), Memory - 38.6 MB (14.81%)

--- my-folder/0494-target-sum/solution.py
+++ my-folder/0494-target-sum/solution.py
@@ -1,22 +1,57 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
         n = len(nums)
-        memo = {}
-        
-        def dp(i,exp):
+
+        @cache
+        def solve(i,exp):
             if i>=n: return 0
-            if i==n-1:
+
+            if i == n-1:
                 count = 0
                 if exp + nums[i] == target: count+=1
                 if exp - nums[i] == target: count+=1
                 return count
             
-            if (i,exp) in memo: return memo[(i,exp)]
-            positive = dp(i+1,exp + nums[i])
-            negative = dp(i+1,exp - nums[i])
-            memo[(i,exp)] = positive + negative
-            return memo[(i,exp)]
-        return dp(0,0)
+            positive = solve(i+1, exp + nums[i])
+            negative = solve(i+1, exp - nums[i])
+            return positive + negative
+
+        return solve(0,0)
+
+
+
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # n = len(nums)
+        # memo = {}
+        
+        # def dp(i,exp):
+        #     if i>=n: return 0
+        #     if i==n-1:
+        #         count = 0
+        #         if exp + nums[i] == target: count+=1
+        #         if exp - nums[i] == target: count+=1
+        #         return count
+            
+        #     if (i,exp) in memo: return memo[(i,exp)]
+        #     positive = dp(i+1,exp + nums[i])
+        #     negative = dp(i+1,exp - nums[i])
+        #     memo[(i,exp)] = positive + negative
+        #     return memo[(i,exp)]
+        # return dp(0,0)
         
 
 
