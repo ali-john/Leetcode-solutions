2273948e226b4693baab1d43839635feb619eae4
Ali John <63426759+ali-john@users.noreply.github.com>
2025-10-10 23:00:45 +0000

    [LeetCode Sync] Runtime - 55 ms (19.36%), Memory - 18.7 MB (19.37%)

--- my-folder/3509-k-th-largest-perfect-subtree-size-in-binary-tree/solution.py
+++ my-folder/3509-k-th-largest-perfect-subtree-size-in-binary-tree/solution.py
@@ -6,32 +6,22 @@
 #         self.right = right
 class Solution:
     def kthLargestPerfectSubtree(self, root: Optional[TreeNode], k: int) -> int:
-        # Check these conditions:
-            # is the height of left and right sub-tree equal?
-            # is right and left sub tree also perfect tree
-        # if yes:
-            # add their sizes in a sizes list
-        # sort the sizes list
-        # return kth-element from list
-        sizes = []
-        def dfs(node):
-            if not node:
-                return (0,True)
-
-            left_node = dfs(node.left)
-            right_node = dfs(node.right)
-
-            if (left_node[1] and right_node[1]) and (left_node[0]==right_node[0]): # heights are equal and both are perfect binary trees
-                sizes.append(2*left_node[0]+1)
-                return (2*left_node[0]+1,True)
-            else:
-                return (left_node[0]+1, False)
+        lengths = []
         
-        dfs(root)
-        sizes = sorted(sizes,reverse=True)
-        if len(sizes)<k:
-            return -1
-        else:
-            return sizes[k-1]
-       
+        def getLengths(root):
+            if not root: return (0, True)
+            left_count, left_dec = getLengths(root.left)
+            right_count, right_dec = getLengths(root.right)
+            
+            isPerfect = False
+            if ( left_count == right_count) and ( left_dec and right_dec):
+                lengths.append(left_count + right_count + 1)
+                isPerfect = True
+            
+            return (left_count + right_count + 1, isPerfect)
+        
+        getLengths(root)
+        lengths.sort(reverse = True)
+        return lengths[k-1] if len(lengths)>= k else -1 
+
 
