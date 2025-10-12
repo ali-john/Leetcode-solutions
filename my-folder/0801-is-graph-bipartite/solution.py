a814cfeafca20e956fadcb07d8b63acc14abf0bf
Ali John <63426759+ali-john@users.noreply.github.com>
2025-10-12 03:05:36 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 18.6 MB (36.93%)

--- my-folder/0801-is-graph-bipartite/solution.py
+++ my-folder/0801-is-graph-bipartite/solution.py
@@ -1,18 +1,20 @@
 class Solution:
     def isBipartite(self, graph: List[List[int]]) -> bool:
+        
         n = len(graph)
         color = [-1]*n
         q = []
         for node in range(n):
-            if color[node]==-1:
-                q.append(node)
+            if color[node] == -1: 
                 color[node] = 0
+                q.append(node)
                 while q:
                     curr = q.pop(0)
                     for nei in graph[curr]:
-                        if color[nei] ==-1:
+                        if color[nei] == -1:
                             color[nei] = 1 - color[curr]
                             q.append(nei)
-                        elif color[nei] == color[curr]: return False
+                        elif color[nei] == color[curr]:
+                            return False
         return True
 
