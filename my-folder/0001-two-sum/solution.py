76ce3fed85dc661e71e2df495ed216071e0c02ea
Ali John <63426759+ali-john@users.noreply.github.com>
2025-10-17 20:36:27 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 18.8 MB (51.05%)

--- my-folder/0001-two-sum/solution.py
+++ my-folder/0001-two-sum/solution.py
@@ -1,23 +1,53 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        table = defaultdict(list)
-
-        for i, num in enumerate(nums):
-            table[num].append(i)
+        n = len(nums)
+        table = defaultdict(int)
+        for i in range(n):
+            table[nums[i]] = i
         
         ans = []
-
         for i, num in enumerate(nums):
             second = target - num
             if second in table:
-                if second == num:
-                    if len(table[second]) > 1:
-                        ans.append(i)
-                        ans.append(table[num].pop())
-                        break
-                else:
+                if table[second] != i:
                     ans.append(i)
-                    ans.append(table[second].pop())
-                    break
+                    ans.append(table[second])
+                    return ans
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # table = defaultdict(list)
+
+        # for i, num in enumerate(nums):
+        #     table[num].append(i)
+        
+        # ans = []
+
+        # for i, num in enumerate(nums):
+        #     second = target - num
+        #     if second in table:
+        #         if second == num:
+        #             if len(table[second]) > 1:
+        #                 ans.append(i)
+        #                 ans.append(table[num].pop())
+        #                 break
+        #         else:
+        #             ans.append(i)
+        #             ans.append(table[second].pop())
+        #             break
                 
-        return ans
+        # return ans
