86eca2d919e9f5c460db7824fd576b0c5b491f9d
Ali John <63426759+ali-john@users.noreply.github.com>
2025-10-18 02:12:30 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.9 MB (28.42%)

--- my-folder/0778-reorganize-string/solution.py
+++ my-folder/0778-reorganize-string/solution.py
@@ -1,33 +1,67 @@
 class Solution:
     def reorganizeString(self, s: str) -> str:
         n = len(s)
-        max_count = 0
         table = defaultdict(int)
-        max_heap = []
-
+        max_freq = 0
+        letter = ''
         for char in s:
-            table[char]+=1
-            if table.get(char)>max_count:
-                max_count=table.get(char) # max count of any char
-        if max_count> math.ceil(n/2):
-            return ""
+            table[char] +=1
+            if table[char] > max_freq:
+                max_freq = table[char]
+                letter = char
+        if max_freq > math.ceil(n / 2): return ""
+
+        ans = ['']*n
+        index = 0
+        while table[letter]!=0:
+            ans[index] = letter
+            table[letter]-=1
+            index+=2
         
-        for key,val in table.items():
-            heapq.heappush(max_heap, (-val,key))
+        for char, count in table.items():
+            while count > 0:
+                if index >= n:
+                    index = 1
+                ans[index] = char
+                index+=2
+                count-=1
+        return ''.join(ans)
+
         
-        output = []
-        prev_char, prev_freq = '', 0
-        while max_heap:
-            freq,char = heapq.heappop(max_heap)
-            output.append(char)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # n = len(s)
+        # table = defaultdict(int)
+        # max_freq = 0
+        # for char in s:
+        #     table[char] +=1
+        #     max_freq = max(max_freq, table[char])
+        
+        # if max_freq > math.ceil(n / 2): return ""
+        
+        # max_heap = []
+        # for char , count in table.items():
+        #     heapq.heappush(max_heap, (-count, char))
+        
+        # output = []
+        # prev_char , prev_freq = '', 0
+        # while max_heap:
+        #     count, char = heapq.heappop(max_heap)
+        #     output.append(char)
 
-            if prev_freq<0:
-                heapq.heappush(max_heap,(prev_freq, prev_char))
+        #     if prev_freq < 0:
+        #         heapq.heappush(max_heap, (prev_freq, prev_char))
             
-            prev_freq, prev_char = freq+1, char
-
-        return "".join(output)
-
+        #     prev_char, prev_freq = char, count + 1
+        # return "".join(output)
             
 
+        
 
