6da5c7f6173454647a1e3532f2bd9dd4e3fb12e2
Ali John <63426759+ali-john@users.noreply.github.com>
2025-10-19 01:05:48 +0000

    [LeetCode Sync] Runtime - 231 ms (39.90%), Memory - 32.9 MB (17.64%)

--- my-folder/4015-split-array-with-minimum-difference/solution.py
+++ my-folder/4015-split-array-with-minimum-difference/solution.py
@@ -1,30 +1,60 @@
 class Solution:
     def splitArray(self, nums: List[int]) -> int:
         n = len(nums)
-
         prefix = [0]*n
         prefix[0] = nums[0]
-        for i in range(1,n):
-            prefix[i] = prefix[i-1] + nums[i]
-        
+        for i in range(1, n):
+            prefix[i] = prefix[i- 1] + nums[i]
         incr = [True]*n
         for i in range(1,n):
-            incr[i] = incr[i-1] and (nums[i-1] < nums[i])
+            incr[i] = incr[i - 1] and (nums[i-1] < nums[i])
         decr = [True]*n
-        for i in range(n-2, -1 , -1):
+        for i in range(n - 2, -1, -1):
             decr[i] = decr[i+1] and (nums[i] > nums[i+1])
-        
         ans = float('inf')
-        valid = False
+
+        for i in range(n-1):
+            if incr[i] and decr[i+1]:
+                diff = abs( (prefix[i]) - ( prefix[-1] - prefix[i] ) )
+                ans = min(ans, diff)
+        return ans if ans!=float('inf') else -1
+
+
+
+
+
+
+
+
+
+
+
+
+        # n = len(nums)
+
+        # prefix = [0]*n
+        # prefix[0] = nums[0]
+        # for i in range(1,n):
+        #     prefix[i] = prefix[i-1] + nums[i]
         
-        # try all split points
-        for i in range(n - 1):
-            if incr[i] and decr[i + 1]:
-                valid = True
-                left_sum = prefix[i]
-                right_sum = prefix[-1] - prefix[i]
-                ans = min(ans, abs(left_sum - right_sum))
+        # incr = [True]*n
+        # for i in range(1,n):
+        #     incr[i] = incr[i-1] and (nums[i-1] < nums[i])
+        # decr = [True]*n
+        # for i in range(n-2, -1 , -1):
+        #     decr[i] = decr[i+1] and (nums[i] > nums[i+1])
         
-        return ans if valid else -1
+        # ans = float('inf')
+        # valid = False
+        
+        # # try all split points
+        # for i in range(n - 1):
+        #     if incr[i] and decr[i + 1]:
+        #         valid = True
+        #         left_sum = prefix[i]
+        #         right_sum = prefix[-1] - prefix[i]
+        #         ans = min(ans, abs(left_sum - right_sum))
+        
+        # return ans if valid else -1
 
         
