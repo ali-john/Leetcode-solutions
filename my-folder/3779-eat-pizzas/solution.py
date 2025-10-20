a5e7f281c9c042a4ba436e8aacadc263a50c6a94
Ali John <63426759+ali-john@users.noreply.github.com>
2025-10-20 22:01:26 +0000

    [LeetCode Sync] Runtime - 179 ms (39.49%), Memory - 43.7 MB (73.85%)

--- my-folder/3779-eat-pizzas/solution.py
+++ my-folder/3779-eat-pizzas/solution.py
@@ -1,13 +1,43 @@
 class Solution:
     def maxWeight(self, pizzas: List[int]) -> int:
+        n = len(pizzas)
         pizzas.sort()
-        days = len(pizzas)//4
-        even_days = days//2
-        odd_days = days-even_days
+        days = n //4
+        even_days = days // 2
+        odd_days = days - even_days
         ans = 0
         for _ in range(odd_days):
-            ans += pizzas.pop()
-        for  _ in range(even_days):
+            ans+=pizzas.pop()
+        for _ in range(even_days):
             pizzas.pop()
-            ans += pizzas.pop()
+            ans+=pizzas.pop()
         return ans
+
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # pizzas.sort()
+        # days = len(pizzas)//4
+        # even_days = days//2
+        # odd_days = days-even_days
+        # ans = 0
+        # for _ in range(odd_days):
+        #     ans += pizzas.pop()
+        # for  _ in range(even_days):
+        #     pizzas.pop()
+        #     ans += pizzas.pop()
+        # return ans
