786ac8d0564a39ab9509829b0c4a5063f662bc9d
Ali John <63426759+ali-john@users.noreply.github.com>
2025-10-20 23:28:55 +0000

    [LeetCode Sync] Runtime - 4880 ms (34.09%), Memory - 45.3 MB (60.23%)

--- my-folder/3760-assign-elements-to-groups-with-constraints/solution.py
+++ my-folder/3760-assign-elements-to-groups-with-constraints/solution.py
@@ -2,55 +2,24 @@ class Solution:
     def assignElements(self, groups: List[int], elements: List[int]) -> List[int]:
 
         n = len(groups)
-        output = [-1]*n
-
         table = defaultdict(int)
-
-        for i, num in enumerate(elements): # map each index
+        for i , num in enumerate(elements):
             if num not in table:
                 table[num] = i
         
-        for i, num in enumerate(groups):
-            index = float('inf')
-            for divisor in range(1,int(num**0.5)+1):
-                if num%divisor == 0:
+        ans = []
+        for i,num in enumerate(groups):
+            possible = len(elements) 
+            for divisor in range(1, int(num**0.5) + 1):
+                if num% divisor == 0:
                     if divisor in table:
-                        index = min(index, table[divisor])
-                    if (num//divisor) in table:
-                        index = min(index, table[num//divisor])
-            if index != float('inf'):
-                output[i] = index
-        return output
+                        possible = min(possible, table[divisor])
+                    if ( num // divisor) in table:
+                        possible = min(possible, table[num//divisor])
 
+            if possible != len(elements):
+                ans.append(possible)
+            else:
+                ans.append(-1)
+        return ans
 
-
-
-
-
-
-
-
-
-
-
-
-        # n = len(groups)
-        # ans = [-1]*n
-        # index_map = defaultdict(int)
-        # for i,num in enumerate(elements):
-        #     if num not in index_map:
-        #         index_map[num] = i
-        
-        # for i,num in enumerate(groups):
-        #     divisor = 1
-        #     index = float("inf")
-        #     for divisor in range(1, int(num**0.5)+1):
-        #         if num%divisor==0:
-        #             if divisor in index_map:
-        #                 index = min(index,index_map[divisor])
-        #             if (num//divisor) in index_map:
-        #                 index = min(index,index_map[num//divisor])
-        #     if index!=float("inf"):
-        #         ans[i] = index
-        # return ans
-        
