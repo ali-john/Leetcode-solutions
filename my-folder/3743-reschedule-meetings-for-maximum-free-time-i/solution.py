79e981350e5be5a98e8a8980beab1784be789a71
Ali John <63426759+ali-john@users.noreply.github.com>
2025-10-21 00:44:13 +0000

    [LeetCode Sync] Runtime - 39 ms (89.23%), Memory - 36.2 MB (68.15%)

--- my-folder/3743-reschedule-meetings-for-maximum-free-time-i/solution.py
+++ my-folder/3743-reschedule-meetings-for-maximum-free-time-i/solution.py
@@ -2,11 +2,10 @@ class Solution:
     def maxFreeTime(self, eventTime: int, k: int, startTime: List[int], endTime: List[int]) -> int:
         n = len(startTime)
         gaps = []
-        gaps.append(startTime[0]-0)
+        gaps.append(startTime[0])
         for i in range(1,n):
             gaps.append(startTime[i] - endTime[i-1])
         gaps.append(eventTime - endTime[-1])
-
         m = len(gaps)
         window = k+1
         curr = sum(gaps[:window])
