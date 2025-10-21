f4eea7a7521a9c8d75fa24ca3df660c75488950e
Ali John <63426759+ali-john@users.noreply.github.com>
2025-10-21 01:57:15 +0000

    [LeetCode Sync] Runtime - 646 ms (5.17%), Memory - 17.7 MB (87.93%)

--- my-folder/3611-construct-the-minimum-bitwise-array-ii/solution.py
+++ my-folder/3611-construct-the-minimum-bitwise-array-ii/solution.py
@@ -1,18 +1,21 @@
 class Solution:
     def minBitwiseArray(self, nums: List[int]) -> List[int]:
-        # The idea is to brute force all 32 bits from lsb and check if unsetting any bit makes the condition nums[i]|nums[i]+1 == nums[i] True. This will be the smallest answer because we are traversing from LSB
         n = len(nums)
-        output = [-1]*n
+        ans = [-1]*n
 
         for i in range(n):
             max_num = bin(nums[i])[2:]
-            for j in range(len(max_num)-1,-1,-1):
-                if list(max_num)[j]=='1':
-                    new_num = list(max_num)
+            max_num = list(max_num)
+            for j in range(len(max_num)-1, -1, -1):
+                if max_num[j] == '1':
+                    new_num = copy.deepcopy(max_num)
                     new_num[j] = '0'
                     new_num = ''.join(new_num)
                     new_num = int(new_num,2)
                     if (new_num| new_num+1) == nums[i]:
-                        output[i] = new_num
+                        ans[i] = new_num
+        return ans
 
-        return output
+
+                
+        
