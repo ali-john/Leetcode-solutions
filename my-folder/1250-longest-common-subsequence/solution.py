ce911a2db8a8ac09d814ed7dcd01b0ea18fdc1cf
Ali John <63426759+ali-john@users.noreply.github.com>
2025-10-22 22:17:38 +0000

    [LeetCode Sync] Runtime - 1219 ms (14.09%), Memory - 309.1 MB (15.81%)

--- my-folder/1250-longest-common-subsequence/solution.py
+++ my-folder/1250-longest-common-subsequence/solution.py
@@ -3,14 +3,40 @@ class Solution:
         n = len(text1)
         m = len(text2)
 
-        dp = [[0 for _ in range(m+1)] for _ in range(n+1)]
-        for i in reversed(range(m)):
-            for j in reversed(range(n)):
-                if text2[i]==text1[j]:
-                    dp[j][i] = 1 + dp[j+1][i+1]
-                else:
-                    dp[j][i] = max(dp[j+1][i],dp[j][i+1])
-        return dp[0][0]
+        @cache
+        def solve(i,j):
+            if i >= n or j >= m: return 0
+
+            ans = 0
+            if text1[i] == text2[j]:
+                ans = 1 + solve(i+1, j+1)
+            else:
+                cond1 = solve(i+1,j)
+                cond2 = solve(i, j+1)
+                ans = max(cond1, cond2)
+            return ans
+        return solve(0,0)
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # n = len(text1)
+        # m = len(text2)
+
+        # dp = [[0 for _ in range(m+1)] for _ in range(n+1)]
+        # for i in reversed(range(m)):
+        #     for j in reversed(range(n)):
+        #         if text2[i]==text1[j]:
+        #             dp[j][i] = 1 + dp[j+1][i+1]
+        #         else:
+        #             dp[j][i] = max(dp[j+1][i],dp[j][i+1])
+        # return dp[0][0]
 
         
 
