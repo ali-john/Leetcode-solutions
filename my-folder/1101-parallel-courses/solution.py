c445a73adb51bd3e4d4e99b9fa94054280ed8cd7
Ali John <63426759+ali-john@users.noreply.github.com>
2025-10-31 04:14:23 +0000

    [LeetCode Sync] Runtime - 11 ms (70.10%), Memory - 19.6 MB (67.63%)

--- my-folder/1101-parallel-courses/solution.py
+++ my-folder/1101-parallel-courses/solution.py
@@ -1,29 +1,75 @@
 class Solution:
     def minimumSemesters(self, n: int, relations: List[List[int]]) -> int:
-        graph = defaultdict(list)
+        graph = {i: [] for i in range(n)}
         in_degree = [0]*n
-        
         for prev, next_ in relations:
-            graph[prev-1].append(next_-1)
-            in_degree[next_-1]+=1
+            graph[prev - 1].append(next_ - 1)
+            in_degree[next_ - 1]+=1
         
         sources = []
         for i,degree in enumerate(in_degree):
-            if degree == 0: sources.append(i)
-
-        count = 0
-        studied = 0
+            if degree == 0:
+                sources.append(i)
+        count, studied = 0, 0 
         while sources:
             count+=1
             next_sources = []
-            for node in sources:
-                studied+=1
-                for child in graph[node]:
+            for source in sources:
+                studied += 1
+                for child in graph[source]:
                     in_degree[child]-=1
-                    if in_degree[child]==0:
+                    if in_degree[child] == 0:
                         next_sources.append(child)
             sources = next_sources
+        
+        return count if studied == n else -1
+
+        
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # graph = defaultdict(list)
+        # in_degree = [0]*n
+        
+        # for prev, next_ in relations:
+        #     graph[prev-1].append(next_- 1)
+        #     in_degree[next_-1]+=1
+        
+        # sources = []
+        # for i,degree in enumerate(in_degree):
+        #     if degree == 0: sources.append(i)
+
+        # count = 0
+        # studied = 0
+        # while sources:
+        #     count+=1
+        #     next_sources = []
+        #     for node in sources:
+        #         studied+=1
+        #         for child in graph[node]:
+        #             in_degree[child]-=1
+        #             if in_degree[child]==0:
+        #                 next_sources.append(child)
+        #     sources = next_sources
            
-        return count if studied==n else -1
+        # return count if studied==n else -1
 
 
