1b50b3db23f0e166fbdce4ba72442d4d0f6e2c31
Ali John <63426759+ali-john@users.noreply.github.com>
2025-10-31 00:05:31 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 18 MB (18.50%)

--- my-folder/0998-check-completeness-of-a-binary-tree/solution.py
+++ my-folder/0998-check-completeness-of-a-binary-tree/solution.py
@@ -6,41 +6,17 @@
 #         self.right = right
 class Solution:
     def isCompleteTree(self, root: Optional[TreeNode]) -> bool:
-        def check(l)->bool:
-            after = False
-            null_val = False
-            for node in l:
-                if null_val:
-                    if node:
-                        return False
-                if node is None:
-                    null_val = True
-            return True
 
-        def bfs(node)->bool:
-            q = [node]
-            while q:
-                s = len(q)
-                current_iter = []
-                space_available = False
-                for i in range(s):
-                    curr = q.pop(0)
-                    if curr:
-                        current_iter.append(curr.left)
-                        current_iter.append(curr.right)
-                        if not(curr.left and curr.right):
-                            space_available = True
-                        q.append(curr.left)
-                        q.append(curr.right)
-                if not check(current_iter):
-                    return False
-                if space_available:
-                    for node in q:
-                        if node:
-                            if node.right or node.left:
-                                return False
-            return True
-
-        return bfs(root)
-
-
+        q =  [root]
+        haveNull = False
+        while q:
+            node = q.pop(0)
+            if node:
+                if haveNull: return False
+                q.append(node.left)
+                q.append(node.right)
+            else:
+                haveNull = True
+        return True
+        
+        
