a8cb69f248581ae4793de180f027a0282e8c1e2a
Ali John <63426759+ali-john@users.noreply.github.com>
2025-11-29 23:03:58 +0000

    [LeetCode Sync] Runtime - 55 ms (11.26%), Memory - 32 MB (9.48%)

--- my-folder/0053-maximum-subarray/solution.py
+++ my-folder/0053-maximum-subarray/solution.py
@@ -6,8 +6,8 @@ class Solution:
 
         for i in range(n):
             curr_sum += nums[i]
-            if curr_sum>max_sum:
-                max_sum = max(max_sum,curr_sum)
-            if curr_sum<0:
+            max_sum = max(max_sum,curr_sum)
+            if curr_sum < 0:
                 curr_sum = 0
+            
         return max_sum
