3640c53d655765677bbf194ea03906394b999d64
Ali John <63426759+ali-john@users.noreply.github.com>
2025-12-09 23:22:12 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 12.5 MB (8.69%)

--- my-folder/0088-merge-sorted-array/solution.cpp
+++ my-folder/0088-merge-sorted-array/solution.cpp
@@ -1,31 +1,19 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        vector<int> num3;
 
-        for (int i = 0; i < m; i++) {
-            num3.push_back(nums1[i]);
+        priority_queue<int, vector<int>, greater<int>> heap;
+        for (int i = 0 ; i< m; i++){
+            heap.push(nums1[i]);
         }
-        for (auto i= nums2.begin(); i<nums2.end(); i++)
-        {
-            num3.push_back(*i);
+        for (int i = 0; i< n;  i++){
+            heap.push(nums2[i]);
         }
-        for (int i=0; i<m+n; i++)
-        {
-            for (int j =i+1; j<m+n; j++)
-            {
-                if (num3[j]< num3[i])
-                {
-                    swap(num3[i],num3[j]);
-                } 
-            }
-        }
+        //cout<< heap.pop() <<endl;
 
-        for (int i=0; i<m+n; i++)
-        {
-            nums1[i] = num3[i];
+        for (int i = 0 ; i < n + m ; i++){
+            nums1[i] = heap.top();
+            heap.pop();
         }
-
-
     }
 };
