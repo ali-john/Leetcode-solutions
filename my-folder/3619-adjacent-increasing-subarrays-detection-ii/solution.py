3e641f11956b42b405c4984379232cb5895948be
Ali John <63426759+ali-john@users.noreply.github.com>
2026-04-26 00:12:32 +0000

    [LeetCode Sync] Runtime - 1443 ms (7.82%), Memory - 47.1 MB (96.35%)

--- my-folder/3619-adjacent-increasing-subarrays-detection-ii/solution.py
+++ my-folder/3619-adjacent-increasing-subarrays-detection-ii/solution.py
@@ -2,34 +2,27 @@ class Solution:
     def maxIncreasingSubarrays(self, nums: List[int]) -> int:
         n = len(nums)
 
-        track = [1]*n
-        for i in range(n-2,-1,-1):
-            if nums[i]<nums[i+1]:
-                track[i] = track[i+1]+1
-        print(track)
+        lengths = [1]*n
+        for i in range(n-2, -1, -1):
+            if nums[i] < nums[i+1]:
+                lengths[i] = lengths[i+1] + 1
+
         def is_possible(k):
             for i in range(n):
-                if track[i]>=k:
-                    if i+k<n:
-                        if track[i+k]>=k:
+                if lengths[i] >= k:
+                    if i + k < n:
+                        if lengths[i+k] >= k:
                             return True
             return False
-        
+
         left = 1
-        right = (n//2)+1
-        max_ans = -1
-        while left<=right:
-            mid = (left+right)//2
+        right = (n//2) + 1
+        ans = -1
+        while left <= right:
+            mid = (left + right) // 2
             if is_possible(mid):
-                max_ans = max(max_ans,mid)
-                left = mid+1
+                ans = max(ans, mid)
+                left = mid + 1
             else:
-                right = mid-1
-        return max_ans
-            
-                
-                    
-                            
-                    
-            
-            
+                right = mid - 1
+        return ans
