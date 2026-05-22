264b00e1280e9b359e59f91b9155daa1b134135c
Ali John <63426759+ali-john@users.noreply.github.com>
2026-05-22 01:23:27 +0000

    [LeetCode Sync] Runtime - 246 ms (30.47%), Memory - 202.6 MB (27.85%)

--- my-folder/2562-count-ways-to-build-good-strings/solution.py
+++ my-folder/2562-count-ways-to-build-good-strings/solution.py
@@ -1,17 +1,15 @@
 class Solution:
     def countGoodStrings(self, low: int, high: int, zero: int, one: int) -> int:
-        # Use dp[i] to record to number of good strings of length i.
-        dp = [1] + [0] * (high)
-        mod = 10 ** 9 + 7
+        MOD = 10**9 + 7
         
-        # Iterate over each length `end`.
-        for end in range(1, high + 1):
-            # check if the current string can be made by append zero `0`s or one `1`s.
-            if end >= zero:
-                dp[end] += dp[end - zero]
-            if end >= one:
-                dp[end] += dp[end - one]
-            dp[end] %= mod
+        @cache
+        def dp(length):
+            if length > high:
+                return 0
+            
+            count = 1 if length >= low else 0
+            count += dp(length + zero) + dp(length + one) % MOD
+
+            return count % MOD
         
-        # Add up the number of strings with each valid length [low ~ high].
-        return sum(dp[low : high + 1]) % mod
+        return dp(0)
